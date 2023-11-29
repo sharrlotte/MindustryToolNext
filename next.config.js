98913e6cd10a6e7b97c84bac55a32687ccf8a0dc
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2023-11-29 15:33:05 +0700

    add image config

--- next.config.js
+++ next.config.js
@@ -23,6 +23,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "mindustry-tool-backend.onrender.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
