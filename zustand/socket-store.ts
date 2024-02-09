03a7ec883d1334e8427824802d23a45cbf9bee16
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-02-09 22:49:19 +0700

    change success color

---
+++ zustand/socket-store.ts
@@ -0,0 +1,20 @@
+import SocketClient from '@/types/data/SocketClient';
+import { create } from 'zustand';
+
+type AuthState = 'loading' | 'authenticated' | 'unauthenticated';
+
+type SocketStore = {
+  socket?: SocketClient;
+  setSocket: (socket: SocketClient) => void;
+  authState: AuthState;
+  setAuthState: (state: AuthState) => void;
+};
+
+const useSocketStore = create<SocketStore>((set) => ({
+  socket: undefined,
+  setSocket: (socket: SocketClient) => set({ socket }),
+  authState: 'loading',
+  setAuthState: (authState) => set({ authState }),
+}));
+
+export default useSocketStore;
