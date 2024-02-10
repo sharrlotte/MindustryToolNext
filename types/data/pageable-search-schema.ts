f93b5fcb1ab3399e295d4965e92580437a337b1c
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-02-10 23:43:08 +0700

    fix: last page param possibly undefined

--- types/data/pageable-search-schema.ts
+++ types/data/pageable-search-schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { sortSchema } from '@/types/data/schemas';
 
 export const searchSchema = z.object({
-  page: z.number().gte(0).default(0).optional(),
+  page: z.number().gte(0).default(0),
   name: z.string().default('').optional(),
   authorId: z.string().default('').optional(),
   tags: z.array(z.string()).default([]).optional(),
@@ -12,5 +12,5 @@ export const searchSchema = z.object({
 export type PaginationSearchQuery = z.infer<typeof searchSchema>;
 
 export type PaginationQuery = {
-  page?: number;
+  page: number;
 };
