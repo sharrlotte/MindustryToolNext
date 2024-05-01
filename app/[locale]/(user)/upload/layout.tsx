e7fb9c7f59615da51362b0c09bb0b16f4d154f9b
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-05-01 22:50:17 +0700

    fix upload layout

--- app/[locale]/(user)/upload/layout.tsx
+++ app/[locale]/(user)/upload/layout.tsx
@@ -47,25 +47,35 @@ export default function Layout({ children }: LayoutProps) {
     },
   ];
 
+  function render({ name, url }: { name: string; url: string }) {
+    return (
+      <Link
+        className={cn(
+          'flex min-w-16 items-center justify-center rounded-sm p-2 opacity-80 transition-colors duration-300 hover:bg-button hover:text-background hover:opacity-100 dark:hover:text-foreground',
+          {
+            'bg-button bg-opacity-100 text-background opacity-100 dark:text-foreground':
+              route === url,
+          },
+        )}
+        href={`/upload/${url}`}
+      >
+        {name}
+      </Link>
+    );
+  }
+
   return (
     <div className="flex h-full w-full flex-col gap-2 overflow-hidden p-4">
       <section className="no-scrollbar flex h-12 w-full items-center gap-2 overflow-y-auto rounded-sm border-border bg-card p-2 text-sm font-bold capitalize">
-        {tabs.map(({ name, url, roles }) => (
-          <ProtectedElement key={name} session={data} all={roles ?? ['USER']}>
-            <Link
-              className={cn(
-                'flex min-w-16 items-center justify-center rounded-sm p-2 opacity-80 transition-colors duration-300 hover:bg-button hover:text-background hover:opacity-100 dark:hover:text-foreground',
-                {
-                  'bg-button bg-opacity-100 text-background opacity-100 dark:text-foreground':
-                    route === url,
-                },
-              )}
-              href={`/upload/${url}`}
-            >
-              {name}
-            </Link>
-          </ProtectedElement>
-        ))}
+        {tabs.map(({ name, url, roles }) =>
+          roles ? (
+            <ProtectedElement key={name} session={data} all={roles}>
+              {render({ name, url })}
+            </ProtectedElement>
+          ) : (
+            render({ name, url })
+          ),
+        )}
       </section>
       {children}
     </div>
