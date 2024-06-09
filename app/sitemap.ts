a9cf9531702abf2a422cf9b5e042dca191864b99
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-06-09 22:25:56 +0700

    fix .env

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,13 @@
-import env from '@/constant/env';
-
 import { MetadataRoute } from 'next';
+
+import env from '@/constant/env';
+import { locales } from '@/locales/client';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = ['schematics', 'maps', 'posts', 'servers', 'upload'];
 
   return routes.reduce<MetadataRoute.Sitemap>((prev, curr) => {
-    for (let lang of env.locales) {
+    for (let lang of locales) {
       prev.push({
         url: `${env.url.base}/${lang}/${curr}`,
         lastModified: new Date(),
