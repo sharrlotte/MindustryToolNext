fb1628cd5de0b4afe46639d8baf9f814922f5eb5
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-07-09 01:58:13 +0700

    better ui

--- app/[locale]/(user)/chat/page.tsx
+++ app/[locale]/(user)/chat/page.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion';
 import {
   ChevronRight,
+  PaperclipIcon,
   SearchIcon,
   SendIcon,
   SmileIcon,
@@ -30,6 +31,9 @@ export default function Page() {
   const { session } = useSession();
 
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const [openMemberPanel, setOpenMemberPanel] = useState<'open' | 'close'>(
+    'close',
+  );
 
   return (
     <div className="flex h-full overflow-hidden flex-col">
@@ -38,45 +42,60 @@ export default function Page() {
           <SearchIcon className="w-5 h-5" />
           #Global
         </div>
-        <MemberPanel />
+        <Button
+          className="p-0"
+          title="Close"
+          variant="icon"
+          onClick={() =>
+            setOpenMemberPanel((prev) => (prev === 'open' ? 'close' : 'open'))
+          }
+        >
+          <UsersIcon className="w-5 h-5" />
+        </Button>
       </div>
-      <div className="grid h-full w-full grid-rows-[1fr_3rem] gap-2 overflow-hidden">
-        <div className="grid h-full w-full overflow-hidden rounded-md px-2">
+      <div className="grid h-full w-full grid-rows-[1fr_auto] overflow-hidden">
+        <div className="grid h-full w-full overflow-hidden rounded-md pl-2">
           <div className="flex h-full flex-col gap-1 overflow-x-hidden">
             {state !== 'connected' ? (
               <LoadingSpinner className="m-auto" />
             ) : (
-              <div
-                className="h-full overflow-y-auto"
-                ref={(ref) => setContainer(ref)}
-              >
-                <MessageList
-                  className="flex flex-col gap-1 h-full"
-                  queryKey={['global']}
-                  room="GLOBAL"
-                  reversed
-                  container={() => container}
-                  params={{ page: 0, size: 40 }}
-                  end={<></>}
-                  noResult={
-                    <div className="h-full w-full flex justify-center font-semibold items-center">
-                      {"Let's start a conversation"}
-                    </div>
-                  }
-                  getFunc={(
-                    _,
-                    params: {
-                      page: number;
-                      size: number;
-                    },
-                  ) =>
-                    socket
-                      .onRoom('GLOBAL')
-                      .await({ method: 'GET_MESSAGE', ...params })
-                  }
+              <div className="h-full overflow-hidden flex">
+                <div
+                  className="h-full overflow-y-auto w-full"
+                  ref={(ref) => setContainer(ref)}
                 >
-                  {(data) => <MessageCard key={data.id} message={data} />}
-                </MessageList>
+                  <MessageList
+                    className="flex flex-col gap-1 h-full"
+                    queryKey={['global']}
+                    room="GLOBAL"
+                    reversed
+                    container={() => container}
+                    params={{ page: 0, size: 40 }}
+                    end={<></>}
+                    noResult={
+                      <div className="h-full w-full flex justify-center font-semibold items-center">
+                        {"Let's start a conversation"}
+                      </div>
+                    }
+                    getFunc={(
+                      _,
+                      params: {
+                        page: number;
+                        size: number;
+                      },
+                    ) =>
+                      socket
+                        .onRoom('GLOBAL')
+                        .await({ method: 'GET_MESSAGE', ...params })
+                    }
+                  >
+                    {(data) => <MessageCard key={data.id} message={data} />}
+                  </MessageList>
+                </div>
+                <MemberPanel
+                  state={openMemberPanel}
+                  setState={setOpenMemberPanel}
+                />
               </div>
             )}
           </div>
@@ -115,16 +134,17 @@ function ChatInput() {
   };
   return (
     <form
-      className="flex h-10 flex-1 gap-1 px-2"
+      className="flex h-14 flex-1 gap-1 px-2 border-t py-2"
       name="text"
       onSubmit={handleFormSubmit}
     >
-      <div className="rounded-md border border-border bg-background px-2 w-full flex items-center">
+      <div className="rounded-md border border-border bg-background px-2 w-full flex items-center gap-2">
         <input
           className="h-full w-full outline-none bg-transparent"
           value={message}
           onChange={(event) => setMessage(event.currentTarget.value)}
         />
+        <PaperclipIcon className="w-6 h-6" />
         <SmileIcon className="w-6 h-6" />
       </div>
       <Button
@@ -140,11 +160,14 @@ function ChatInput() {
   );
 }
 
-function MemberPanel() {
+type MemberPanelProps = {
+  state: 'open' | 'close';
+  setState: (func: (prev: 'open' | 'close') => 'open' | 'close') => void;
+};
+
+function MemberPanel({ state, setState }: MemberPanelProps) {
   const { socket } = useSocket();
   const isMedium = useMediaQuery('(min-width: 640px)');
-
-  const [state, setState] = useState<'open' | 'close'>('close');
 
   const { data } = useQuery({
     queryKey: ['member-count', 'GLOBAL'],
@@ -155,39 +178,27 @@ function MemberPanel() {
   });
 
   return (
-    <div>
-      <motion.div
-        className="h-full absolute right-0 sm:relative overflow-y-auto top-0 flex flex-col items-start bg-card"
-        animate={state}
-        variants={{
-          open: {
-            width: isMedium ? 300 : '100%',
-          },
-          close: {
-            width: 0,
-          },
-        }}
-      >
-        <Button
-          title="Close"
-          variant="icon"
-          onClick={() =>
-            setState((prev) => (prev === 'open' ? 'close' : 'open'))
-          }
-        >
-          <ChevronRight />
-        </Button>
-        {state === 'open' &&
-          data?.map((user) => <MemberCard key={user.id} user={user} />)}
-      </motion.div>
+    <motion.div
+      className="h-full absolute right-0 sm:relative overflow-y-auto top-0 flex flex-col items-start bg-card"
+      animate={state}
+      variants={{
+        open: {
+          width: isMedium ? 300 : '100%',
+        },
+        close: {
+          width: 0,
+        },
+      }}
+    >
       <Button
-        className="p-0"
         title="Close"
         variant="icon"
         onClick={() => setState((prev) => (prev === 'open' ? 'close' : 'open'))}
       >
-        <UsersIcon className="w-5 h-5" />
+        <ChevronRight />
       </Button>
-    </div>
+      {state === 'open' &&
+        data?.map((user) => <MemberCard key={user.id} user={user} />)}
+    </motion.div>
   );
 }
