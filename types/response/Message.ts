082e05f22be25fed02e6fe34c03b9be2ed1f90eb
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-07-21 23:02:52 +0700

    adopt api changes

--- types/response/Message.ts
+++ types/response/Message.ts
@@ -1,7 +1,62 @@
+const MAX_GROUP_SIZE = 10;
+
 export interface Message {
   id: string;
   room: string;
   userId: string;
-  content: string[];
+  content: string;
+  attachments: string[];
   createdAt: number;
+}
+export interface MessageGroup {
+  id: string;
+  room: string;
+  userId: string;
+  contents: {
+    text: string;
+    attachments: string[];
+  }[];
+  createdAt: number;
+}
+
+export function groupMessage(messages: Message[]): MessageGroup[] {
+  const result: MessageGroup[] = [];
+
+  for (const message of messages) {
+    if (result.length === 0) {
+      result.push({
+        id: message.id,
+        room: message.room,
+        userId: message.userId,
+        contents: [{ text: message.content, attachments: message.attachments }],
+        createdAt: message.createdAt,
+      });
+    } else {
+      const lastGroup = result[result.length - 1];
+      if (
+        new Date(message.createdAt).getTime() -
+          new Date(lastGroup.createdAt).getTime() <=
+          300000 &&
+        message.userId === lastGroup.userId &&
+        lastGroup.contents.length < MAX_GROUP_SIZE
+      ) {
+        lastGroup.contents.push({
+          text: message.content,
+          attachments: message.attachments,
+        });
+      } else {
+        result.push({
+          id: message.id,
+          room: message.room,
+          userId: message.userId,
+          contents: [
+            { text: message.content, attachments: message.attachments },
+          ],
+          createdAt: message.createdAt,
+        });
+      }
+    }
+  }
+
+  return result;
 }
