f8c76c76a4c280b5166a1757f2e59190939bcb1d
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-07-29 23:17:01 +0700

    fix oversized grid-pagination

--- hooks/use-search-query.ts
+++ hooks/use-search-query.ts
@@ -3,11 +3,27 @@ import { z } from 'zod';
 
 import { QuerySchema } from '@/query/query';
 
+const groupParamsByKey = (params: URLSearchParams) =>
+  params.entries().reduce<Record<string, any>>((acc, tuple) => {
+    const [key, val] = tuple;
+    if (acc.hasOwnProperty(key)) {
+      if (Array.isArray(acc[key])) {
+        acc[key] = [...acc[key], val];
+      } else {
+        acc[key] = [acc[key], val];
+      }
+    } else {
+      acc[key] = val;
+    }
+
+    return acc;
+  }, {});
+
 export default function useSearchQuery<T extends QuerySchema>(
   schema: T,
 ): z.infer<typeof schema> {
   const query = useSearchParams();
-  const data = Object.fromEntries(query.entries());
+  const data = groupParamsByKey(query);
 
   const result = schema.parse(data);
 
