ae594da61473f37a64f0a7d96dfb4789e9bb8e16
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-08-01 22:44:54 +0700

    fix page flashing on schematic/map

--- app/[locale]/(user)/maps/page.tsx
+++ app/[locale]/(user)/maps/page.tsx
@@ -1,14 +1,7 @@
-'use client';
-
 import { Metadata } from 'next';
-import { useState } from 'react';
 
-import ResponsiveInfiniteScrollGrid from '@/components/common/responsive-infinite-scroll-grid';
-import MapPreviewCard from '@/components/map/map-preview-card';
-import NameTagSearch from '@/components/search/name-tag-search';
-import PreviewSkeleton from '@/components/skeleton/preview-skeleton';
-import useSearchPageParams from '@/hooks/use-search-page-params';
-import { useSearchTags } from '@/hooks/use-tags';
+import MapList from '@/app/[locale]/(user)/maps/map-list';
+import env from '@/constant/env';
 import getServerAPI from '@/query/config/get-server-api';
 import getMaps from '@/query/map/get-maps';
 
@@ -28,34 +21,5 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function MapPage() {
-  const { map } = useSearchTags();
-  const params = useSearchPageParams();
-  const [container, setContainer] = useState<HTMLDivElement | null>(null);
-
-  return (
-    <div className="flex h-full flex-col gap-4 overflow-hidden p-4">
-      <NameTagSearch tags={map} />
-      <div
-        className="relative flex h-full flex-col overflow-auto "
-        ref={(ref) => setContainer(ref)}
-      >
-        <ResponsiveInfiniteScrollGrid
-          params={params}
-          queryKey={['maps']}
-          getFunc={getMaps}
-          container={() => container}
-          skeleton={{
-            amount: 20,
-            item: <PreviewSkeleton />,
-          }}
-          itemMinWidth={320}
-          itemMinHeight={352}
-          contentOffsetHeight={112}
-          gap={8}
-        >
-          {(data) => <MapPreviewCard key={data.id} map={data} />}
-        </ResponsiveInfiniteScrollGrid>
-      </div>
-    </div>
-  );
+  return <MapList />;
 }
