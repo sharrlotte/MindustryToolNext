e83fbe2fb007f95f105b1f3d1fef00d3b35c7d10
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-08-10 20:37:55 +0700

    report error link

--- components/common/error-screen.tsx
+++ components/common/error-screen.tsx
@@ -4,6 +4,7 @@ import { notFound } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 import { useI18n } from '@/locales/client';
+import Tran from '@/components/common/tran';
 
 export default function ErrorScreen({
   reset,
@@ -25,11 +26,19 @@ export default function ErrorScreen({
       <h2 className="text-base">{message}</h2>
       <div className="grid items-center justify-center gap-2">
         <Button title={t('try-again')} onClick={() => reset()}>
-          {t('try-again')}
+          <Tran text="try-again" />
         </Button>
         <Button title={t('refresh')} onClick={() => window.location.reload()}>
-          {t('refresh')}
+          <Tran text="refresh" />
         </Button>
+        <a
+          className="text-lg text-brand hover:text-brand"
+          href="https://discord.gg/DCX5yrRUyp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Tran text="report-error-at" />
+        </a>
       </div>
     </div>
   );
