2287feda4ed668b40c61400122810f6691084125
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-08-13 10:14:55 +0700

    add konva

--- app/[locale]/(user)/logic/page.tsx
+++ app/[locale]/(user)/logic/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import dynamic from 'next/dynamic';
 import React from 'react';
 
-export default function Page() {
-  return <div>Page</div>;
+const NoSSRComponent = dynamic(
+  () => import('@/app/[locale]/(user)/logic/editor'),
+  {
+    ssr: false,
+  },
+);
+
+export default function TestsPage() {
+  return <NoSSRComponent />;
 }
