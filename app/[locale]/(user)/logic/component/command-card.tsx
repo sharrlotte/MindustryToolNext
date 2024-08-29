1360618b1951857d5ee7b715b5a19985dd5253c1
meohexa1a <meohexa.1.a@gmail.com>
2024-08-29 19:16:38 +0700

    Preview new model

---
+++ app/[locale]/(user)/logic/component/command-card.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from "react";
+import Command from "../command";
+import { Stage, Layer, Group, Rect, Line } from 'react-konva';
+
+
+export default function CommandCard({ commands }: { commands: Command[] }) {
+
+	return (
+		<Layer>
+			
+		</Layer>
+	);
+}
