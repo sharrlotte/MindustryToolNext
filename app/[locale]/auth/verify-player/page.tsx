609e56358f6d80ab88ffa000628aa44696aec93c
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-09-04 21:24:59 +0700

    more message

--- app/[locale]/auth/verify-player/page.tsx
+++ app/[locale]/auth/verify-player/page.tsx
@@ -60,6 +60,7 @@ function Verify({ token }: { token: string }) {
   return (
     <div className="flex h-full items-center justify-center text-3xl font-bold text-success">
       <Tran text="token.verified" />
+      <Tran text="token.you-can-go-back-to-game-now" />
     </div>
   );
 }
