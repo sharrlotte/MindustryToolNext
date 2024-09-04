98b22908e22031dec52ff30d20eec1c255f5c18c
meohexa1a <meohexa.1.a@gmail.com>
2024-09-04 21:21:17 +0700

    h chỉ có nước tách ra...

--- app/[locale]/(user)/logic/_component/icon.tsx
+++ app/[locale]/(user)/logic/_component/icon.tsx
@@ -5,8 +5,8 @@ export function Delete({ x, y, size, strokeWidth, fill, onClick }:
   { x: number; y: number; size: number; strokeWidth: number; fill: string; onClick?: () => void }) {
   return (
     <Group x={x} y={y}>
-      <Line points={[0, 0, size, size]} stroke={fill} strokeWidth={strokeWidth} perfectDrawEnabled={false} />
-      <Line points={[0, size, size, 0]} stroke={fill} strokeWidth={strokeWidth} perfectDrawEnabled={false} />
+      <Line points={[0, 0, size, size]} stroke={fill} lineCap='round' strokeWidth={strokeWidth} perfectDrawEnabled={false} />
+      <Line points={[0, size, size, 0]} stroke={fill} lineCap='round' strokeWidth={strokeWidth} perfectDrawEnabled={false} />
       <Rect width={size} height={size} onClick={() => { if (onClick) { onClick() } }} />
     </Group>
   );
@@ -17,9 +17,9 @@ export function Copy({ x, y, size, strokeWidth, fill, onClick }:
   const one = size / 4; const two = size - one;
   return (
     <Group x={x} y={y}>
-      <Rect x={0} y={0} width={two} height={two} stroke={fill} fill={'white'}  strokeWidth={strokeWidth} cornerRadius={size / 10} perfectDrawEnabled={false} />
-      <Rect x={one} y={one} width={two} height={two} stroke={fill} fill={'white'} strokeWidth={strokeWidth} cornerRadius={size / 10} perfectDrawEnabled={false} />
-      <Rect width={size} height={size} onClick={onClick}/>
+      <Rect x={0} y={0} width={two} height={two} stroke={fill} fill={'white'} strokeWidth={strokeWidth} cornerRadius={size / 7} perfectDrawEnabled={false} />
+      <Rect x={one} y={one} width={two} height={two} stroke={fill} fill={'white'} strokeWidth={strokeWidth} cornerRadius={size / 7} perfectDrawEnabled={false} />
+      <Rect width={size} height={size} onClick={onClick} />
     </Group>
   );
 }
