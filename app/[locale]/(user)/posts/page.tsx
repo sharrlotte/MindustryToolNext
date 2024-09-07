52cedb473174da17399388cc97f2da6ff0c27d41
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-09-07 19:15:16 +0700

    dedupe member

--- app/[locale]/(user)/posts/page.tsx
+++ app/[locale]/(user)/posts/page.tsx
@@ -8,12 +8,18 @@ import NameTagSearch from '@/components/search/name-tag-search';
 import useSearchPageParams from '@/hooks/use-search-page-params';
 import { useSearchTags } from '@/hooks/use-tags';
 import { getPosts } from '@/query/post';
+import InternalLink from '@/components/common/internal-link';
+import Tran from '@/components/common/tran';
+import { UserIcon, UploadIcon } from 'lucide-react';
+import env from '@/constant/env';
 
 export default function PostsPage() {
   const { post } = useSearchTags();
   const params = useSearchPageParams();
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
 
+  const uploadLink = `${env.url.base}/upload/post`;
+  const myPostLink = `${env.url.base}/users/me`;
   return (
     <div className="flex h-full w-full flex-col gap-4 p-4">
       <NameTagSearch tags={post} />
@@ -34,6 +40,24 @@ export default function PostsPage() {
         >
           {(data) => <PostPreviewCard key={data.id} post={data} />}
         </ResponsiveInfiniteScrollGrid>
+      </div>
+      <div className="flex gap-2">
+        <InternalLink
+          variant="button-secondary"
+          title="my-post"
+          href={myPostLink}
+        >
+          <UserIcon />
+          <Tran text="my-post" />
+        </InternalLink>
+        <InternalLink
+          variant="button-secondary"
+          title="upload-post"
+          href={uploadLink}
+        >
+          <UploadIcon />
+          <Tran text="upload-post" />
+        </InternalLink>
       </div>
     </div>
   );
