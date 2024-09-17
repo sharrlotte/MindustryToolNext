5d21d9431740522d58de9d8b1c48a4150eb97c6e
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-09-17 21:39:01 +0700

    fix startCommand

--- types/request/UpdateInternalServerRequest.ts
+++ types/request/UpdateInternalServerRequest.ts
@@ -13,7 +13,7 @@ export const PutInternalServerSchema = z.object({
   name: z.string().min(1).max(50),
   description: z.string().min(1).max(200),
   mode: z.enum(InternalServerModes).default('SURVIVAL'),
-  startCommand: z.string().max(1000).optional().nullable(),
+  startCommand: z.string().max(1000).optional(),
 });
 
 export type PutInternalServerRequest = z.infer<typeof PutInternalServerSchema>;
