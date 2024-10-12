684acfee40b5b4454e700f735a8f612d655a2262
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-10-13 03:37:26 +0700

    remove sleep

--- app/[locale]/(user)/plugins/page.tsx
+++ app/[locale]/(user)/plugins/page.tsx
@@ -1,8 +1,6 @@
 import PageClient from '@/app/[locale]/(user)/plugins/page.client';
-import { sleep } from '@/lib/utils';
 import React from 'react';
 
 export default async function Page() {
-  await sleep(10);
   return <PageClient />;
 }
