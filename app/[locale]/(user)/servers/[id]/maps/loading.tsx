667b787e8174d1f1b387a29c2e81416b118d8d87
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-10-17 04:12:59 +0700

    fix server map page flickering

--- app/[locale]/(user)/servers/[id]/maps/loading.tsx
+++ app/[locale]/(user)/servers/[id]/maps/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-2 p-2">
+    <div className="flex flex-col gap-2">
       <div className="flex gap-2">
         <Skeleton className="h-14 w-full" />
       </div>
