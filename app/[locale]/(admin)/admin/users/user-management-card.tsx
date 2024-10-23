88924193b5880180a9444f8ab30f461d7fd5a167
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-10-23 15:34:36 +0700

    UI improve

--- app/[locale]/(admin)/admin/users/user-management-card.tsx
+++ app/[locale]/(admin)/admin/users/user-management-card.tsx
@@ -12,8 +12,8 @@ type Props = {
 
 function _UserManagementCard({ user }: Props) {
   return (
-    <div className="grid w-full grid-cols-[1fr_3rem] gap-2 bg-card px-4 py-2">
-      <div className="flex justify-start space-x-2 overflow-hidden">
+    <div className="grid w-full grid-cols-[1fr_10rem] gap-2 bg-card px-4 py-2">
+      <div className="flex justify-between space-x-2 overflow-hidden">
         <UserAvatar user={user} />
         <CopyButton className="w-full justify-start overflow-hidden hover:bg-transparent" data={user.id} variant="ghost" content={user.id}>
           <h3>{user.name}</h3>
