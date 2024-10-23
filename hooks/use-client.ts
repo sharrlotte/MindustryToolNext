e0952a2ece0400b4ef596b82ea54d43d7ac51421
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-10-23 18:29:44 +0700

    increase request timeout for client req

--- hooks/use-client.ts
+++ hooks/use-client.ts
@@ -5,5 +5,7 @@ import { AxiosInstance } from 'axios';
 import axiosInstance from '@/query/config/config';
 
 export default function useClientApi(): AxiosInstance {
+  axiosInstance.defaults.timeout = 20000;
+
   return axiosInstance;
 }
