cf103c3944f845bab59d088ade58d116befb7fe5
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-10-25 17:33:37 +0700

    lazyload youtube embed

--- components/common/youtube-embed.tsx
+++ components/common/youtube-embed.tsx
@@ -1,30 +1,49 @@
+'use client';
+
 import Tran from '@/components/common/tran';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 type Props = {
   url: string;
 };
 
-const YOUTUBE_VIDEO_REGEX =
-  /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+const YOUTUBE_VIDEO_REGEX = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
 
 export default function YoutubeEmbed({ url }: Props) {
   const match = url.match(YOUTUBE_VIDEO_REGEX);
   const id = match ? match[1] : null;
 
+  const [show, setShow] = useState(false);
+
   if (!id) {
     return <Tran text="invalid-youtube-url" />;
   }
 
+  useEffect(() => {
+    function onLoad() {
+      setShow(true);
+    }
+
+    window.addEventListener('mousemove', onLoad);
+    window.addEventListener('touchmove', onLoad);
+
+    return () => {
+      window.removeEventListener('mousemove', onLoad);
+      window.removeEventListener('touchmove', onLoad);
+    };
+  });
+
   return (
     <div className="relative h-fit w-full pb-[56.25%]">
-      <iframe
-        title="YouTube video player"
-        className="absolute left-0 top-0 h-full w-full rounded-md"
-        src={`https://www.youtube.com/embed/${id}?autoplay=1&loop=1&controls=1`}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; controls; loop;"
-        allowFullScreen
-      />
+      {show && (
+        <iframe
+          title="YouTube video player"
+          className="absolute left-0 top-0 h-full w-full rounded-md"
+          src={`https://www.youtube.com/embed/${id}?autoplay=1&loop=1&controls=1`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; controls; loop;"
+          allowFullScreen
+        />
+      )}
     </div>
   );
 }
