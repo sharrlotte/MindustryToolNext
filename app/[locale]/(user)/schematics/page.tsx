767c29f30af3ca3c162f753514495ae75e7272f2
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-11-12 16:00:24 +0700

    remove unknown error on schematic and post page

--- app/[locale]/(user)/schematics/page.tsx
+++ app/[locale]/(user)/schematics/page.tsx
@@ -2,30 +2,12 @@ import { Metadata } from 'next/dist/types';
 
 import SchematicList from '@/app/[locale]/(user)/schematics/schematic-list';
 import env from '@/constant/env';
-import { getSchematics } from '@/query/schematic';
-import { serverApi } from '@/action/action';
-import { isError } from '@/lib/utils';
 
 export const revalidate = 3600;
 
 export async function generateMetadata(): Promise<Metadata> {
-  const schematics = await serverApi((axios) => getSchematics(axios, { page: 0, size: 1 }));
-
-  if (isError(schematics)) {
-    return {
-      title: 'Error',
-    };
-  }
-
-  const schematic = schematics[0];
-
   return {
     title: `${env.webName} > Schematic`,
-    description: schematic.name,
-    openGraph: {
-      title: schematic.name,
-      images: `${env.url.image}schematic-previews/${schematic.id}${env.imageFormat}`,
-    },
   };
 }
 
