412c46dee78abdfdc404d8f43907a65a68c8bc34
AlexNe <truonggiang190689@gmail.com>
2024-11-13 03:29:54 +0700

    test modal

--- app/[locale]/user-display.tsx
+++ app/[locale]/user-display.tsx
@@ -6,6 +6,9 @@ import { Skeleton } from '@/components/ui/skeleton';
 import UserAvatar from '@/components/user/user-avatar';
 import UserRoleCard from '@/components/user/user-role';
 import { useSession } from '@/context/session-context.client';
+import useToggle from '@/hooks/use-state-toggle';
+import Modal from '@/layout/modal';
+import { LogOut } from 'lucide-react';
 
 export function UserDisplay() {
   return (
@@ -18,19 +21,45 @@ export function UserDisplay() {
 }
 export function Internal() {
   const { session, state } = useSession();
+  const modal = useToggle();
 
   if (state === 'authenticated' && session) {
     return (
-      <div className="flex h-16 items-center justify-between rounded-sm bg-card p-1">
-        <div className="flex items-center justify-center gap-1">
-          <UserAvatar className="h-12 w-12" user={session} url="/users/@me" />
-          <div className="grid p-1">
-            <span className="capitalize">{session.name}</span>
-            <UserRoleCard roles={session.roles} />
+      <>
+        <div className="flex h-16 items-center justify-between rounded-sm bg-card p-1">
+          <div className="flex items-center justify-center gap-1">
+            <UserAvatar className="h-12 w-12" user={session} url="/users/@me" />
+            <div className="grid p-1">
+              <span className="capitalize">{session.name}</span>
+              <UserRoleCard roles={session.roles} />
+            </div>
+          </div>
+          <div className="justify-start pr-1" onClick={modal.toggle}>
+            <LogOut className="size-5" />
           </div>
         </div>
-        <LogoutButton className="justify-start pr-1" />
-      </div>
+        {
+          //#region modal
+        }
+        <Modal isOpen={modal.isOpen} onClose={modal.close}>
+          <div className="flex w-full flex-col gap-4">
+            <h1 className="text-2xl font-bold text-red-500">Log out</h1>
+            <p>Are you sure you want to log out?</p>
+            <div className="flex justify-end gap-2">
+              <button className="rounded border border-gray-500 px-2 py-1 transition-colors hover:bg-gray-500 hover:text-white" onClick={modal.toggle}>
+                Cancel
+              </button>
+              <button className="flex items-center rounded bg-sky-500 px-2 py-1 text-white transition-colors hover:bg-red-600">
+                <LogoutButton className="justify-start pr-1" />
+                <span>Log out</span>
+              </button>
+            </div>
+          </div>
+        </Modal>
+        {
+          //#endregion
+        }
+      </>
     );
   }
 
