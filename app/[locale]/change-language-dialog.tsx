799210b1bf83d4780a7b94eb9dc8731408d0d4b1
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-11-16 13:24:25 +0700

    dont auto play youtube

--- app/[locale]/change-language-dialog.tsx
+++ app/[locale]/change-language-dialog.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import ComboBox from '@/components/common/combo-box';
-import { Dialog, DialogContent, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { useChangeLocale, useI18n } from '@/i18n/client';
 
 import { useLocaleStore } from '@/zustand/locale-store';
 import { Locale, locales } from '@/i18n/config';
 import Tran from '@/components/common/tran';
+import { Hidden } from '@/components/common/hidden';
 
 export function ChangeLanguageDialog() {
   const { currentLocale } = useLocaleStore();
@@ -23,6 +24,9 @@ export function ChangeLanguageDialog() {
         <Tran text="switch-language" />
       </DialogTrigger>
       <DialogContent className="p-6">
+        <Hidden>
+          <DialogTitle />
+        </Hidden>
         <ComboBox<Locale>
           value={{ label: t(currentLocale), value: currentLocale }}
           values={locales.map((value: Locale) => ({
