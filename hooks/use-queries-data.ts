25fbe32fab632a63a22f976a014572fce9ba7d5f
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-11-21 08:24:50 +0700

    fix notification

--- hooks/use-queries-data.ts
+++ hooks/use-queries-data.ts
@@ -5,7 +5,7 @@ import { QueryKey, useQueryClient } from '@tanstack/react-query';
 export default function useQueriesData() {
   const queryClient = useQueryClient();
 
-  const invalidateByKey = useCallback((...queryKeys: QueryKey[]) => queryKeys.forEach((queryKey) => queryClient.invalidateQueries({ queryKey })), [queryClient]);
+  const invalidateByKey = useCallback((...queryKeys: QueryKey[]) => queryKeys.forEach((queryKey) => queryClient.invalidateQueries({ queryKey, exact: false })), [queryClient]);
 
   return {
     invalidateByKey,
