8c47217970a7f35dccf776c8fbaced080d905476
AlexNe <truonggiang190689@gmail.com>
2024-11-22 08:11:09 +0700

    update input node #169

--- app/[locale]/(user)/logic/nodes/TextUpdaterNode.tsx
+++ app/[locale]/(user)/logic/nodes/TextUpdaterNode.tsx
@@ -5,16 +5,24 @@ interface TextUpdaterNodeProps {
   isConnectable: boolean;
 }
 
+const adjustInputWidth = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const tempSpan = document.createElement('span');
+  tempSpan.style.visibility = 'hidden';
+  tempSpan.style.position = 'absolute';
+  tempSpan.style.whiteSpace = 'pre';
+  tempSpan.textContent = e.target.value || ' ';
+  document.body.appendChild(tempSpan);
+  e.target.style.width = `${tempSpan.offsetWidth}px`;
+  document.body.removeChild(tempSpan);
+};
+
 export const TextUpdaterNode: React.FC<TextUpdaterNodeProps> = ({ data, isConnectable }) => {
   return (
-    <div className="p-1.5  border border-[#1a192b] rounded min-w-[150px] bg-white text-xs">
+    <div className="p-1.5 border border-[#1a192b] rounded min-w-[150px] bg-white text-xs">
       <Handle type="target" position={Position.Top} isConnectable={isConnectable} />
       <div className="flex flex-col gap-1 text-black">
         <label htmlFor="text">{data.label}:</label>
-        <input
-          value={data.value}
-          className="nodrag bg-black text-white block w-full rounded border-0 px-3 py-1.5  placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm/6 outline-none"
-        />
+        <input value={data.value} className="nodrag bg-black text-white block w-full rounded border-0 px-3 py-1.5 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm outline-none" />
       </div>
       <Handle type="source" position={Position.Bottom} id="b" isConnectable={isConnectable} />
     </div>
@@ -23,23 +31,26 @@ export const TextUpdaterNode: React.FC<TextUpdaterNodeProps> = ({ data, isConnec
 
 export const SetNode: React.FC<TextUpdaterNodeProps> = ({ isConnectable }) => {
   return (
-    <div className="p-1.5  border border-[#1a192b] rounded min-w-[150px] bg-white text-xs">
+    <div className="p-1.5 border border-[#1a192b] rounded bg-white text-xs">
       <Handle type="source" position={Position.Right} isConnectable={isConnectable} />
       <div className="flex flex-col gap-1 text-black">
         <p>Set</p>
-        <div className="bg-black gap-1 flex items-center w-fit">
-          <input
-            value={'resuit'}
-            className="nodrag bg-black text-white block rounded border-0 px-3 py-1.5 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6 outline-none underline"
-            style={{ width: `${'resuit'.length + 1}ch`, textDecoration: 'underline white' }}
-          />
+        <div className="bg-black flex items-center rounded">
+          <div className="px-3">
+            <input
+              type="text"
+              className="nodrag bg-black text-white block w-7 rounded py-1.5 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm outline-none"
+              onChange={adjustInputWidth}
+            />
+          </div>
           <p className="text-white">=</p>
-          <input
-            value={'0'}
-            className="nodrag bg-black text-white block rounded border-0 px-3 py-1.5 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm/6 outline-none underline"
-            size={1}
-            style={{ textDecoration: 'underline white' }}
-          />
+          <div className="px-3">
+            <input
+              type="text"
+              className="nodrag w-7 bg-black rounded text-white block py-1.5 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm outline-none"
+              onChange={adjustInputWidth}
+            />
+          </div>
         </div>
       </div>
       <Handle type="target" position={Position.Left} isConnectable={isConnectable} />
