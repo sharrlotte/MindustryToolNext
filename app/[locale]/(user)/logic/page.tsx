06ca15d5216ab6dd7f63f5a2f566dd4a5075e120
AlexNe <truonggiang190689@gmail.com>
2024-11-23 03:02:38 +0700

    add remove node #169

--- app/[locale]/(user)/logic/page.tsx
+++ app/[locale]/(user)/logic/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useCallback, useState } from 'react';
-import ReactFlow, { Background, Controls, EdgeChange, MiniMap, NodeChange, addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
+import ReactFlow, { Background, Controls, Edge, EdgeChange, MiniMap, Node, NodeChange, addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow';
 import 'reactflow/dist/style.css';
 
 import { SmartBezierEdge } from '@tisoap/react-flow-smart-edge';
@@ -34,6 +34,10 @@ import {
 } from './nodes/TextUpdaterNode';
 import initialNodes from './nodes/nodes';
 
+const edgeTypes = {
+  smart: SmartBezierEdge,
+};
+
 const nodeTypes = {
   textUpdater: TextUpdaterNode,
   waitNode: WaitNode,
@@ -60,26 +64,55 @@ const nodeTypes = {
   printNode: PrintNode,
 };
 
-const edgeTypes = {
-  smart: SmartBezierEdge,
-};
-
 export default function Page() {
   return <Flow />;
 }
 
 function Flow() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
 
   const onNodeChange = useCallback((changes: NodeChange[]) => setNodes((nds) => applyNodeChanges(changes, nds)), [setNodes]);
 
-  const onEdgeChange = useCallback((changes: EdgeChange[]) => setEdges((eds) => applyEdgeChanges(changes, eds) as any), [setEdges]);
+  const onEdgeChange = useCallback((changes: EdgeChange[]) => setEdges((eds) => applyEdgeChanges(changes, eds)), [setEdges]);
 
-  const onEdgeConnect = useCallback((params: any) => setEdges((eds) => addEdge({ ...params, animated: true, type: 'smart' }, eds) as any), [setEdges]);
+  const onEdgeConnect = useCallback((params: any) => setEdges((eds) => addEdge({ ...params, animated: true, type: 'smart' }, eds)), [setEdges]);
+
+  const onNodesDelete = useCallback((deleted: Node[]) => {
+    setNodes((nds) => nds.filter((node) => !deleted.some((d) => d.id === node.id)));
+    setEdges((eds) => eds.filter((edge) => !deleted.some((d) => d.id === edge.source || d.id === edge.target)));
+  }, []);
+
+  const onEdgesDelete = useCallback((deleted: Edge[]) => {
+    setEdges((eds) => eds.filter((edge) => !deleted.some((d) => d.id === edge.id)));
+  }, []);
+
+  const onNodeContextMenu = useCallback(
+    (event: React.MouseEvent, node: Node) => {
+      event.preventDefault();
+    },
+    [setNodes, setEdges],
+  );
+
+  const onEdgeContextMenu = useCallback((event: React.MouseEvent, edge: Edge) => {
+    event.preventDefault();
+    setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+  }, []);
 
   return (
-    <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodeChange} nodeTypes={nodeTypes} edgeTypes={edgeTypes} onEdgesChange={onEdgeChange} onConnect={onEdgeConnect}>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodeChange}
+      onEdgesChange={onEdgeChange}
+      onConnect={onEdgeConnect}
+      onNodesDelete={onNodesDelete}
+      onEdgesDelete={onEdgesDelete}
+      nodeTypes={nodeTypes}
+      edgeTypes={edgeTypes}
+      onNodeContextMenu={onNodeContextMenu}
+      onEdgeContextMenu={onEdgeContextMenu}
+    >
       <MiniMap />
       <Controls />
       <Background />
