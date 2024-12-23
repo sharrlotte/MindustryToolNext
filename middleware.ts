ad924d5bc688f81185ac055cad2b16f8e3927b8c
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-12-23 23:42:32 +0700

    update middleware source matcher to exclude favicon.ico and improve routing configuration

--- middleware.ts
+++ middleware.ts
@@ -56,7 +56,7 @@ export function middleware(request: NextRequest) {
 export const config: MiddlewareConfig = {
   matcher: [
     {
-      source: '/((?!api|_next|assets|_next/image|favicon.ico|robots.txt|.*sitemap|ads|en|vi|ru|kr|cn|uk|jp).*)',
+      source: '/((?!api|_next|assets|_next|favicon.ico|robots.txt|.*sitemap|ads|en|vi|ru|kr|cn|uk|jp).*)',
     },
   ],
 };
