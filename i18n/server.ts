f5d82d19c3bdc46118e22580b1d8d19604707504
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-12-26 09:23:06 +0700

    fix: update i18next integration to use chained backend for improved translation handling

--- i18n/server.ts
+++ i18n/server.ts
@@ -1,12 +1,13 @@
 import { createInstance } from 'i18next';
-import HttpApi from 'i18next-http-backend';
+import Backend from 'i18next-chained-backend';
+import { ChainedBackendOptions } from 'i18next-chained-backend';
 import { initReactI18next } from 'react-i18next/initReactI18next';
 
 import { Locale, defaultLocale, getOptions, locales } from '@/i18n/config';
 
 const initI18next = async (language: Locale, namespace: string) => {
   const i18nInstance = createInstance();
-  await i18nInstance.use(initReactI18next).use(HttpApi).init(getOptions(language, namespace));
+  await i18nInstance.use(initReactI18next).use(Backend).init<ChainedBackendOptions>(getOptions(language, namespace));
 
   return i18nInstance;
 };
