44399b80e8fdc4b04c576822f8b74ff0ab727c2c
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-12-27 17:01:20 +0700

    fix: update useI18n hook to use correct cookie name for language checks

--- i18n/client.ts
+++ i18n/client.ts
@@ -64,13 +64,13 @@ export function useI18n(namespace: string | string[] = 'common', options?: any) 
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-      if (cookies.i18next === language) return;
+      if (cookies[cookieName] === language) return;
 
-      console.log(['cookie', language, cookies.i18next]);
+      console.log(['cookie', language, cookies[cookieName]]);
 
       setCookie(cookieName, language, { path: '/' });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [language, cookies.i18next]);
+    }, [language, cookies[cookieName]]);
   }
   return ret;
 }
