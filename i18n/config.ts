c7d680cfe06bc49a3ab6523ee8ee3aaca84d042d
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-12-30 03:40:43 +0700

    fix: remove unused imports and clean up i18n configuration

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,7 +1,6 @@
-import { BackendModule, InitOptions, ReadCallback, ResourceLanguage } from 'i18next';
+import { InitOptions } from 'i18next';
 import { ChainedBackendOptions } from 'i18next-chained-backend';
 import HttpApi, { HttpBackendOptions } from 'i18next-http-backend';
-import { use } from 'react';
 
 import env from '@/constant/env';
 
@@ -12,7 +11,7 @@ export const locales = env.locales;
 export type Locale = (typeof locales)[number];
 
 export type TranslateFunction = (key: string, args?: Record<string, any>) => string;
-export const defaultNamespace: string | string[] = ['common', 'tags'];
+export const defaultNamespace = ['common', 'tags'];
 
 export function getOptions(lng = defaultLocale, ns = defaultNamespace) {
   const options: InitOptions<ChainedBackendOptions> = {
@@ -24,7 +23,7 @@ export function getOptions(lng = defaultLocale, ns = defaultNamespace) {
     defaultNS: defaultNamespace,
     ns,
     backend: {
-      backends: [typeof window === 'undefined' ? CBackend : HttpApi],
+      backends: [HttpApi],
       backendOptions: [
         {
           loadPath: `${env.url.api}/translations/{{lng}}/{{ns}}`,
@@ -39,17 +38,4 @@ export function getOptions(lng = defaultLocale, ns = defaultNamespace) {
   };
 
   return options;
-}
-
-class CBackend implements BackendModule<any> {
-  type: 'backend' = 'backend';
-
-  init(services?: any, options?: HttpBackendOptions): void {}
-  read(language: string, namespace: string, callback: ReadCallback): void {
-    const result = fetch(`${env.url.api}/translations/${language}/${namespace}`).then((r) => r.json());
-
-    const data = use(result);
-
-    callback(null, data);
-  }
 }
