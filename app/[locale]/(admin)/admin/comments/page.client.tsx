be83403f0cc2518a6d01cc4c7e9b5009d41c7392
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-02 18:39:15 +0700

    feat: enhance comments display with user avatars, roles, and markdown support

--- app/[locale]/(admin)/admin/comments/page.client.tsx
+++ app/[locale]/(admin)/admin/comments/page.client.tsx
@@ -15,6 +15,12 @@ import { ItemPaginationQuery } from '@/query/search-query';
 
 import { getAllCommentCount, getAllComments } from '@/query/comment';
 import { Comment } from '@/types/response/Comment';
+import Markdown from '@/components/markdown/markdown';
+import { RelativeTime } from '@/components/common/relative-time';
+import { Skeleton } from '@/components/ui/skeleton';
+import ColorAsRole from '@/components/user/color-as-role';
+import UserAvatar from '@/components/user/user-avatar';
+import { getUser } from '@/query/user';
 
 
 export default function Client() {
@@ -33,6 +39,7 @@ export default function Client() {
         <ScrollContainer className="relative flex h-full flex-col" ref={(ref) => setContainer(ref)}>
           <ListLayout>
             <InfinitePage
+            className='flex flex-col gap-1'
               params={params}
               queryKey={['comments']}
               queryFn={getAllComments}
@@ -44,7 +51,8 @@ export default function Client() {
           </ListLayout>
           <GridLayout>
             <GridPaginationList
-              params={params}
+            className='flex flex-col gap-1'
+params={params}
               queryKey={['comments']}
               queryFn={getAllComments}
             >
@@ -67,8 +75,31 @@ type CommentCardProps = {
 }
 
 function CommentCard({comment} : CommentCardProps){
+const { userId, content, createdAt } = comment;
 
-  return     <div className="flex items-center gap-2 p-2">
-    {comment.content}
-  </div>;
+  const { data } = useClientQuery({
+    queryKey: ['users', userId],
+    queryFn: (axios) => getUser(axios, { id: userId }),
+  });
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex w-full gap-2 text-wrap items-center text-xs">
+        {data ? <UserAvatar user={data} url /> : <Skeleton className="flex size-8 min-h-8 min-w-8 items-center justify-center rounded-full border border-border capitalize" />}
+        <div className="overflow-hidden">
+          <div className="flex gap-2 items-baseline">
+            {data ? (
+              <ColorAsRole className="font-semibold capitalize text-base" roles={data.roles}>
+                {data.name}
+              </ColorAsRole>
+            ) : (
+              <Skeleton className="h-4 max-h-1 w-24" />
+            )}
+            <RelativeTime className="text-muted-foreground" date={new Date(createdAt)} />
+          </div>
+        </div>
+      </div>
+      <Markdown className="ml-10 text-sm">{content}</Markdown>
+    </div>
+  )  
 }
