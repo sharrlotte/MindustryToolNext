c50ae925e5635265dfabc819378aa3c4b76f58c4
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-03 14:20:51 +0700

    cleanup

--- app/[locale]/(user)/credit/page.tsx
+++ app/[locale]/(user)/credit/page.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 export default function Page() {
   return (
-    <div>
-      Credit to:
-      <p>
-        bend-n: https://github.com/bend-n/design-it
-        <br />
-        Mindustry official discord server
-      </p>
+    <div className="p-4">
+      <h1>Credit to:</h1>
+      <ul>
+        <li>
+          <a className="text-emerald-400" href="https://github.com/bend-n/design-it">
+            bend-n
+          </a>
+        </li>
+        <li>
+          <a className="text-emerald-400" href="https://discord.gg/BYgU3jFNc6">
+            Mindustry official discord server
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
