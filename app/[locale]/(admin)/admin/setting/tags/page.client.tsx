af8ce0d28269061983d4c69efa629cc8fad86d13
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-05 00:54:58 +0700

    refactor: enhance tag loading with error and loading screens, and increase axios timeout

--- app/[locale]/(admin)/admin/setting/tags/page.client.tsx
+++ app/[locale]/(admin)/admin/setting/tags/page.client.tsx
@@ -2,6 +2,28 @@
 
 import React from 'react';
 
+import ErrorScreen from '@/components/common/error-screen';
+import LoadingScreen from '@/components/common/loading-screen';
+
+import useClientApi from '@/hooks/use-client';
+import { getTags } from '@/query/tag';
+
+import { useQuery } from '@tanstack/react-query';
+
 export default function PageClient() {
-  return <div>page.client</div>;
+  const axios = useClientApi();
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['tags'],
+    queryFn: () => getTags(axios),
+  });
+
+  if (isError) {
+    return <ErrorScreen error={error} />;
+  }
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
--- hooks/use-client.ts
+++ hooks/use-client.ts
@@ -5,7 +5,7 @@ import { AxiosInstance } from 'axios';
 import axiosInstance from '@/query/config/config';
 
 export default function useClientApi(): AxiosInstance {
-  axiosInstance.defaults.timeout = 20000;
+  axiosInstance.defaults.timeout = 60000;
 
   return axiosInstance;
 }
