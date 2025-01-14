8b1adf8192e71d18283cd8b317e5d3734dfbbec1
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-14 21:07:11 +0700

    fix loading skeleton for plugin and post page

---
+++ components/post/post-card-skeleton.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function PostCardSkeleton() {
+  return <Skeleton className="relative flex h-[212px] w-full flex-col gap-2 rounded-md bg-card" />;
+}
--- components/post/upload-post-preview-card.tsx
+++ components/post/upload-post-preview-card.tsx
@@ -2,6 +2,7 @@ import React, { HTMLAttributes } from 'react';
 
 import InternalLink from '@/components/common/internal-link';
 import IdUserCard from '@/components/user/id-user-card';
+
 import { cn, getImageById } from '@/lib/utils';
 import { Post } from '@/types/response/Post';
 
@@ -14,15 +15,15 @@ function InternalUploadPostPreviewCard({ className, post, ...rest }: UploadPostP
   const { id, title, userId, createdAt } = post;
 
   return (
-    <div style={{ backgroundImage: `url(${getImageById('posts', firstImage)})` }} className={cn('relative flex flex-col rounded-lg border border-border', className)} {...rest}>
+    <div style={{ backgroundImage: `url(${getImageById('posts', firstImage)})` }} className={cn('h-[212px] relative flex flex-col rounded-lg border border-border', className)} {...rest}>
       <div className="flex h-full flex-col justify-between gap-2 p-4 backdrop-brightness-50">
         <InternalLink href={`/admin/posts/${id}`}>
-          <span className="flex text-2xl">{title}</span>
+          <p className="text-2xl w-full text-ellipsis overflow-hidden line-clamp-2">{title}</p>
         </InternalLink>
         <div className="flex flex-col gap-2">
           <div>
             <IdUserCard id={userId} />
-            <span>{new Date(createdAt).toLocaleString()}</span>
+            <span className="text-muted-foreground">{new Date(createdAt).toLocaleString()}</span>
           </div>
         </div>
       </div>
