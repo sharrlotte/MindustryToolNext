c96a01adf711683d2fc4e0d7228648d681300c38
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-16 13:46:00 +0700

    tag page

--- app/[locale]/(admin)/admin/setting/tags/page.tsx
+++ app/[locale]/(admin)/admin/setting/tags/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import PageClient from '@/app/[locale]/(admin)/admin/setting/tags/page.client';
 
+import LoadingScreen from '@/components/common/loading-screen';
+
+export const experimental_ppr = true;
+
 export default function Page() {
-  return <PageClient />;
+  return (
+    <Suspense fallback={<LoadingScreen />}>
+      <PageClient />
+    </Suspense>
+  );
 }
