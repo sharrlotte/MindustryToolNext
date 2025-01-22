0187fa0e1e87ed74d803537530cfdae44f318252
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-22 12:06:28 +0700

    merge vanilla and mod tag

--- hooks/use-tags.ts
+++ hooks/use-tags.ts
@@ -4,17 +4,41 @@ import { TagType } from '@/constant/constant';
 import useClientApi from '@/hooks/use-client';
 import { getTags } from '@/query/tag';
 import { Mod } from '@/types/response/Mod';
+import TagGroup, { AllTagGroup } from '@/types/response/TagGroup';
 
 import { useQuery } from '@tanstack/react-query';
 
 export default function useTags(type: TagType, mod?: Mod) {
   const axios = useClientApi();
   const { data } = useQuery({
-    queryKey: ['tags', mod?.id],
-    queryFn: async () => getTags(axios, mod?.id),
+    queryKey: ['tags'],
+    queryFn: async () => getTags(axios),
   });
 
-  const tags = useMemo(() => (data && type in data ? data[type].filter((v) => v.values.length > 0) : []), [data, type]);
+  const { data: mods } = useQuery({
+    queryKey: ['tags', mod?.id],
+    queryFn: async () => (mod ? getTags(axios, mod.id) : ({} as unknown as AllTagGroup)),
+  });
+
+  const tags = useMemo(() => merge(validateTags(data, type), validateTags(mods, type)), [data, mods, type]);
 
   return tags;
+}
+
+function validateTags(data: AllTagGroup | undefined, type: TagType) {
+  return data && type in data ? data[type].filter((v) => v.values.length > 0) : [];
+}
+
+function merge(tags1: TagGroup[], tags2: TagGroup[]): TagGroup[] {
+  tags2.forEach((value) => {
+    const group = tags1.find((v) => v.name === value.name);
+
+    if (group) {
+      group.values.concat(value.values);
+    } else {
+      tags1.push(value);
+    }
+  });
+
+  return tags1;
 }
