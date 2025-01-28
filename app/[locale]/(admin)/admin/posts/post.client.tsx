a66c1004165ecf057b3417ebc25cf2479b0cd887
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-28 15:14:33 +0700

    refactor: remove unused imports from loading and post.client components

--- app/[locale]/(admin)/admin/posts/post.client.tsx
+++ app/[locale]/(admin)/admin/posts/post.client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React from 'react';
 
 import InfinitePage from '@/components/common/infinite-page';
 import ScrollContainer from '@/components/common/scroll-container';
