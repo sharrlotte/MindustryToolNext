6a5a5bfc9e4d83e4c0a3822d5c8893da3dd21429
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-02-05 08:31:06 +0700

    fix: enhance layout and styling of NameTagSearchSkeleton for improved consistency and responsiveness; add sleep function in page component

--- components/search/name-tag-search-skeleton.tsx
+++ components/search/name-tag-search-skeleton.tsx
@@ -10,12 +10,12 @@ export default function NameTagSearchSkeleton() {
   const { t } = useI18n();
 
   return (
-    <div className="flex justify-center gap-1.5 rounded-md overflow-hidden">
-      <div className="h-11 flex w-full items-center justify-center gap-2 rounded-sm bg-card px-2 shadow-md">
+    <div className="flex h-11 justify-center gap-1.5 rounded-md">
+      <div className="flex h-11 w-full items-center justify-center gap-2 rounded-sm bg-card px-3 shadow-md">
         <SearchIcon className="size-5 shrink-0" />
-        <input placeholder={t('search-by-name')} className="h-full w-full bg-transparent hover:outline-none focus:outline-none" />
+        <input placeholder={t('search-by-name')} className="h-full w-full bg-card hover:outline-none focus:outline-none" />
       </div>
-      <button className="inline-flex h-11 items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-70 text-nowrap border-border hover:bg-accent hover:text-accent-foreground px-4 py-2 bg-card rounded-sm border shadow-md">
+      <button className="inline-flex h-11 items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none border-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-70 text-nowrap hover:bg-accent hover:text-accent-foreground px-4 py-2 bg-card rounded-sm border shadow-md">
         <FilterIcon className="size-5" />
       </button>
     </div>
