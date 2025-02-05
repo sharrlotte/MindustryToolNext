72c1686486a012d4e14b2df9ef4269147b469a0f
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-02-05 08:23:10 +0700

    fix: update NameTagSearchSkeleton styles for improved layout consistency and responsiveness

--- components/search/name-tag-search-skeleton.tsx
+++ components/search/name-tag-search-skeleton.tsx
@@ -10,12 +10,12 @@ export default function NameTagSearchSkeleton() {
   const { t } = useI18n();
 
   return (
-    <div className="flex justify-center gap-2">
-      <div className="h-10 flex w-full items-center justify-center gap-2 rounded-sm border px-2 shadow-md">
+    <div className="flex justify-center gap-1.5 rounded-md overflow-hidden">
+      <div className="h-11 flex w-full items-center justify-center gap-2 rounded-sm bg-card px-2 shadow-md">
         <SearchIcon className="size-5 shrink-0" />
         <input placeholder={t('search-by-name')} className="h-full w-full bg-transparent hover:outline-none focus:outline-none" />
       </div>
-      <button className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-70 text-nowrap border-border bg-transparent hover:bg-accent hover:text-accent-foreground px-4 py-2 h-10 border shadow-md">
+      <button className="inline-flex h-11 items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-70 text-nowrap border-border hover:bg-accent hover:text-accent-foreground px-4 py-2 bg-card rounded-sm border shadow-md">
         <FilterIcon className="size-5" />
       </button>
     </div>
