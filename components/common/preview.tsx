594b936c2867394a3dd689e7b998b8f07252135f
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-02-05 12:20:43 +0700

    fix: improve layout and styling of Preview component for enhanced consistency and responsiveness

--- components/common/preview.tsx
+++ components/common/preview.tsx
@@ -9,7 +9,7 @@ type CardProps = React.HTMLAttributes<HTMLDivElement>;
 
 export function Preview({ className, children, ...props }: CardProps) {
   return (
-    <div className={cn('group overflow-hidden relative flex min-h-preview-height min-w-[min(100vw,var(--preview-size))] bg-card max-w-[calc(var(--preview-size)*2)] flex-col rounded-md shadow-md animate-appear p-2', className)} {...props}>
+    <div className={cn('group overflow-hidden relative flex min-h-preview-height min-w-[min(100vw,var(--preview-size))] bg-card max-w-[calc(var(--preview-size)*2)] flex-col rounded-md shadow-md animate-appear', className)} {...props}>
       {children}
     </div>
   );
@@ -27,13 +27,13 @@ type ImageProps = React.HTMLAttributes<HTMLImageElement> & {
 } & Parameters<typeof Image>[0];
 
 export function PreviewImage({ className, src, errorSrc, alt, ...props }: ImageProps) {
-  return <FallbackImage className={cn('aspect-square object-cover w-full bg-black rounded-sm', className)} src={src} errorSrc={errorSrc} alt={alt} width={224} height={224} priority={props.loading == 'eager' ? true : undefined} {...props} />;
+  return <FallbackImage className={cn('aspect-square object-cover w-full bg-black', className)} src={src} errorSrc={errorSrc} alt={alt} width={224} height={224} priority={props.loading == 'eager' ? true : undefined} {...props} />;
 }
 
 type ActionsProps = React.HTMLAttributes<HTMLDivElement>;
 
 export function PreviewActions({ className, children }: ActionsProps) {
-  return <section className={cn('grid w-full grid-flow-col justify-center gap-2 [grid-auto-columns:minmax(0,1fr)]', className)}>{children}</section>;
+  return <section className={cn('grid w-full grid-flow-col justify-center gap-2 [grid-auto-columns:minmax(0,1fr)] p-2', className)}>{children}</section>;
 }
 
 type DescriptionProps = React.HTMLAttributes<HTMLDivElement>;
