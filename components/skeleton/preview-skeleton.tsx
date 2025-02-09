31090df97f375fe44a9046c0ce57b2d4239ad5a1
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-02-09 15:57:17 +0700

    fix preview skeleton

--- components/skeleton/preview-skeleton.tsx
+++ components/skeleton/preview-skeleton.tsx
@@ -6,7 +6,7 @@ export default function PreviewSkeleton() {
   return (
     <div className="flex min-h-preview-height min-w-[min(100vw,var(--preview-size))] max-w-[calc(var(--preview-size)*2)] flex-col rounded-md overflow-hidden bg-card backdrop-filter backdrop-blur-sm shadow-md">
       <Skeleton className="h-full aspect-square w-full rounded-none" />
-      <div className="flex h-28 flex-col items-center justify-center p-1" />
+      <div className="flex h-28 flex-col items-center justify-center" />
     </div>
   );
 }
