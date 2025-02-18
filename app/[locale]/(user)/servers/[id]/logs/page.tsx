92632af19bc34457158073151f406902aaaf8d58
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-02-18 13:05:06 +0700

    remove extra s

--- app/[locale]/(user)/servers/[id]/logs/page.tsx
+++ app/[locale]/(user)/servers/[id]/logs/page.tsx
@@ -16,7 +16,7 @@ type Props = {
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { locale } = await params;
   const { t } = await getTranslation(locale);
-  const title = t('logs');
+  const title = t('log');
 
   return {
     title: formatTitle(title),
