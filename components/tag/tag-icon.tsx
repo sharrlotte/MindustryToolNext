3aa42668b3fd20fb66bd69f60a1ba68d73035045
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-02-18 09:48:02 +0700

    fix tag icon bg

--- components/tag/tag-icon.tsx
+++ components/tag/tag-icon.tsx
@@ -6,5 +6,5 @@ export default function TagIcon({ className, children }: { className?: string, c
   if (!children) {
     return undefined;
   }
-  return <Image loading="lazy" className={cn("size-4 shrink-0 rounded-md overflow-hidden bg-background", className)} src={children} width={28} height={28} alt={children} />;
+  return <Image loading="lazy" className={cn("size-4 shrink-0 rounded-md overflow-hidden", className)} src={children} width={28} height={28} alt={children} />;
 }
