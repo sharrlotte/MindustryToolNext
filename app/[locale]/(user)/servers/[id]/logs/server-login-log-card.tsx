063a87c59d1a6d404b39b81469c32e89d21329a1
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-02-25 23:24:30 +0700

    hide uuid and ip

--- app/[locale]/(user)/servers/[id]/logs/server-login-log-card.tsx
+++ app/[locale]/(user)/servers/[id]/logs/server-login-log-card.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+
 import ColorText from '@/components/common/color-text';
+import { EyeIcon, EyeOffIcon } from '@/components/common/icons';
 import { RelativeTime } from '@/components/common/relative-time';
 
 import { cn } from '@/lib/utils';
@@ -10,11 +15,20 @@ type ServerLoginLogCardProps = {
 };
 
 export default function ServerLoginLogCard({ data: { name, uuid, ip, createdAt }, index }: ServerLoginLogCardProps) {
+  const [showUuid, setShowUuid] = useState(false);
+  const [showIp, setShowIp] = useState(false);
+
   return (
-    <div className={cn('p-4 grid-cols-1 grid md:grid-cols-4', index % 2 === 0 ? 'bg-card/50' : 'bg-card')}>
+    <div className={cn('p-4 grid-cols-1 grid md:grid-cols-4 gap-2', index % 2 === 0 ? 'bg-card/50' : 'bg-card')}>
       <ColorText text={name} />
-      <span>{uuid}</span>
-      <span>{ip}</span>
+      <div className="flex items-center gap-2">
+        <button onClick={() => setShowUuid((prev) => !prev)}>{showUuid ? <EyeOffIcon /> : <EyeIcon />}</button>
+        {showUuid ? uuid : '*'.repeat(27)}
+      </div>
+      <div className="flex items-center gap-2">
+        <button onClick={() => setShowIp((prev) => !prev)}>{showIp ? <EyeOffIcon /> : <EyeIcon />}</button>
+        {showIp ? ip : ip.replaceAll(/\d/g, '*')}
+      </div>
       <RelativeTime date={new Date(createdAt)} />
     </div>
   );
