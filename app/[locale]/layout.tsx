5be242bdacd3f588394ed61c616be0217b591103
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-01 21:04:36 +0700

    refactor: remove loading spinner from MessageContainer and adjust import statement in layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import { Inter, Noto_Sans_KR } from 'next/font/google';
 import localFont from 'next/font/local';
-import Script from 'next/script';
+import Script from 'next/script'
 import NextTopLoader from 'nextjs-toploader';
 import React from 'react';
 import ReactDOM from 'react-dom';
