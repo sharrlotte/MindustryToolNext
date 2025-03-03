bcd290f11b553fee5b27e8272fee9a6da40d7193
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-03 21:34:10 +0700

    fix: change refetchOnMount to 'always' for consistent data retrieval

--- hooks/use-message-query.ts
+++ hooks/use-message-query.ts
@@ -57,7 +57,7 @@ export default function useMessageQuery<P extends MessageQuery>(room: string, pa
     getNextPageParam,
     getPreviousPageParam,
     enabled: state === 'connected',
-    refetchOnMount: true,
+    refetchOnMount: 'always',
   });
 
   return data;
