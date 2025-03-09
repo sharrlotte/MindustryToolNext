6dc572d14acd2200c8c6cabf811c6a60657b7155
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-09 20:04:41 +0700

    feat: implement static params generation and disable dynamic parameters for improved performance

--- app/[locale]/(user)/docs/[...path]/layout.tsx
+++ app/[locale]/(user)/docs/[...path]/layout.tsx
@@ -10,6 +10,30 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/
 
 import { cn } from '@/lib/utils';
 
+export const dynamicParams = false;
+export const revalidate = false;
+export function generateStaticParams() {
+  const localeFolders = p.join(process.cwd(), 'docs');
+  const locales = fs.readdirSync(localeFolders);
+
+  return locales.flatMap((locale) => {
+    const docsFolderPath = p.join(localeFolders, locale);
+    const docsFolders = fs.readdirSync(docsFolderPath);
+
+    return docsFolders.flatMap((folder) => {
+      const docsPath = p.join(docsFolderPath, folder);
+      const docs = fs.readdirSync(docsPath).filter((file) => file.endsWith('.mdx'));
+
+      const paths = docs.map((file) => file.replace('.mdx', ''));
+
+      return paths.map((path) => ({
+        path: [folder, path],
+        locale,
+      }));
+    });
+  });
+}
+
 export default async function Layout({ children, params }: { children: ReactNode; params: Promise<{ path: string[]; locale: string }> }) {
   const { locale, path } = await params;
   const [category, docs] = path;
@@ -27,6 +51,7 @@ export default async function Layout({ children, params }: { children: ReactNode
           const content = fs.readFileSync(p.join(docsFolder, file)).toString();
           const index = content.indexOf('\n');
           const header = content.slice(0, index === -1 ? content.length : index).replace('#', '');
+
           return { title: header, docs: file.replace('.mdx', '') };
         });
 
@@ -36,19 +61,21 @@ export default async function Layout({ children, params }: { children: ReactNode
     }),
   );
 
+  console.log(category);
+
   return (
     <div className="p-4 grid grid-cols-[20rem_auto] divide-x h-full">
       <div className="pr-4 space-y-4">
         <Accordion className="space-y-4" type="single" collapsible defaultValue={category}>
-          {data.map(({ meta, titles, cat }, index) => (
-            <AccordionItem key={index} value={cat}>
+          {data.map(({ meta, titles, cat }) => (
+            <AccordionItem key={cat} value={cat}>
               <AccordionTrigger className="text-xl py-0 justify-start text-start text-nowrap">{meta.title}</AccordionTrigger>
               <AccordionContent>
                 {titles.map((value) => (
                   <div key={value.docs}>
                     <div className="border-l"></div>
                     <InternalLink
-                      href={`/${locale}/docs/${category}/${value.docs}`}
+                      href={`/${locale}/docs/${cat}/${value.docs}`}
                       className={cn('text-lg px-4 py-2 rounded-md hover:bg-muted/50 text-muted-foreground', {
                         'text-brand': value.docs === docs,
                       })}
