833c1a32a56ddbb1994c12169808103243c90c1d
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-11 23:06:33 +0700

    feat: adjust padding in ScrollContainer and update InternalLink hover styles for improved UI consistency

--- app/[locale]/docs/page.tsx
+++ app/[locale]/docs/page.tsx
@@ -22,14 +22,14 @@ export default async function Page({ params }: { params: Promise<{ locale: strin
   }
 
   return (
-    <ScrollContainer className="p-4">
+    <ScrollContainer className="p-2">
       <Accordion type="single" collapsible>
         {docs.map(({ category, title, docs }) => (
           <AccordionItem key={category} value={category}>
             <AccordionTrigger className="py-1 px-3 bg-card rounded-sm text-lg">{title}</AccordionTrigger>
             <AccordionContent className="p-0">
               {docs.map(({ header, filename }) => (
-                <InternalLink key={filename} href={`/${locale}/docs/${category}/${filename}`} className="text-sm px-4 py-2 rounded-sm hover:bg-muted/50 text-muted-foreground">
+                <InternalLink key={filename} href={`/${locale}/docs/${category}/${filename}`} className="text-sm px-4 hover:text-brand py-2 rounded-sm hover:bg-muted/50 text-muted-foreground">
                   {header}
                 </InternalLink>
               ))}
