bf7921872d4753a66f8d8991a853a1bb3fd49b70
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-19 18:17:20 +0700

    feat: reduce gap spacing in schematic page layout for improved UI consistency

--- app/[locale]/(main)/schematics/page.tsx
+++ app/[locale]/(main)/schematics/page.tsx
@@ -39,11 +39,10 @@ export default async function Page() {
   const uploadLink = `${env.url.base}/upload/schematic`;
 
   return (
-    <div className="flex h-full flex-col gap-4 overflow-hidden p-2">
+    <div className="flex h-full flex-col gap-2 overflow-hidden p-2">
       <NameTagSearch type="schematic" />
-
       <Client />
-      <div className="flex flex-wrap items-center gap-4 justify-between">
+      <div className="flex flex-wrap items-center gap-2 justify-between">
         <InternalLink variant="button-secondary" href={uploadLink}>
           <UploadIcon />
           <Tran text="upload-schematic" />
