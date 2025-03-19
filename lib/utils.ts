a58bdaae33df1ccae352fc1e8ed388615f2898f1
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-19 15:44:14 +0700

    feat: integrate Sentry for enhanced error reporting in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,6 +12,8 @@ import { ChartData, Metric } from '@/types/response/Metric';
 import { Session } from '@/types/response/Session';
 import TagGroup from '@/types/response/TagGroup';
 
+import * as Sentry from '@sentry/nextjs';
+
 export function uuid() {
   return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) => (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16));
 }
@@ -24,6 +26,7 @@ export function isError<T extends Record<string, any> | number>(req: T | ApiErro
   if (isError) {
     try {
       reportError(axiosInstance, getLoggedErrorMessage(req as any));
+      Sentry.captureException(req);
     } catch (e) {
       console.error(e);
     }
