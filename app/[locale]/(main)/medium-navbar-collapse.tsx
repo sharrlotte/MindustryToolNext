330b0e695fd388bc0582e329e481890e8ac47d95
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-25 21:56:35 +0700

    fix: enhance layout structure in MediumNavbarCollapse by adjusting class names

--- app/[locale]/(main)/medium-navbar-collapse.tsx
+++ app/[locale]/(main)/medium-navbar-collapse.tsx
@@ -25,9 +25,9 @@ export default function MediumNavbarCollapse({ children }: Props) {
   const { visible } = useNavBar();
 
   return (
-    <div className="hidden sm:flex">
+    <div className="hidden sm:flex h-full">
       <motion.div
-        className={cn('relative gap-2 h-full overflow-hidden bg-card min-w-nav w-full flex-col p-1', { 'p-2': visible })}
+        className={cn('relative gap-2 h-full overflow-hidden bg-card min-w-nav w-full flex-col p-1 flex', { 'p-2': visible })}
         variants={sidebarVariants}
         initial={visible ? { width: sidebarVariants.open.width } : { width: sidebarVariants.closed.width }}
         animate={visible ? 'open' : 'closed'}
