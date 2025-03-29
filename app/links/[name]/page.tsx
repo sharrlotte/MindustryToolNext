4640de4cfcff9d62f1ccd8fafc4b613cd15268c2
junghamin1909 <nctgame1998@gmail.com>
2025-03-30 00:00:49 +0900

    Update page.tsx
    New Horizon Mod
    Mindustry Tool

--- app/links/[name]/page.tsx
+++ app/links/[name]/page.tsx
@@ -4,11 +4,25 @@ import { notFound, redirect } from 'next/navigation';
 import { formatTitle } from '@/lib/utils';
 
 const links: Record<string, { href: string; title: string; description: string }> = {
-  'mindustry-tool-vn': { href: 'https://discord.gg/9qMxQZm6Wb', title: 'Discord MindustryTool Việt Name', description: 'Tham gia vào máy chủ của Mindustry Việt Nam' },
+  'mindustry-vietnam': {
+    href: 'https://discord.gg/9qMxQZm6Wb',
+    title: 'Discord MindustryTool Việt Nam',
+    description: 'Tham gia vào máy chủ của Mindustry Việt Nam',
+  },
+  'mindustry-tool': {
+    href: 'https://discord.gg/YGuBQ3g9fQ',
+    title: 'Mindustry Tool',
+    description: 'Join our active Mindustry community to connect with fellow players, share experiences, get support, and collaborate on exciting development projects. Let’s grow together!',
+  },
+  'new-horizon': {
+    href: 'https://discord.gg/aRRwAMDeEs',
+    title: 'New Horizon Mod',
+    description: 'Join our New Horizon Mod support and development server',
+  },
 };
 
-export async function generateMetadata({ params }: { params: Promise<{ name: string }> }): Promise<Metadata> {
-  const { name } = await params;
+export async function generateMetadata({ params }: { params: { name: string } }): Promise<Metadata> {
+  const { name } = params;
 
   const item = links[name];
 
@@ -28,8 +42,8 @@ export async function generateMetadata({ params }: { params: Promise<{ name: str
   };
 }
 
-export default async function Page({ params }: { params: Promise<{ name: string }> }) {
-  const { name } = await params;
+export default async function Page({ params }: { params: { name: string } }) {
+  const { name } = params;
 
   const item = links[name];
 
