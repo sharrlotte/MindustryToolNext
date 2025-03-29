ff2c10a351119e8175b4d11b099b31130b63eac8
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-29 20:19:35 +0700

    feat: refactor navigation bar and add dynamic link redirection page

--- app/[locale]/(main)/navigation.tsx
+++ app/[locale]/(main)/navigation.tsx
@@ -1,26 +1,18 @@
-'use client';
-
-import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 
-import { NavBarProvider } from '@/context/navbar-context';
-import IsSmall from '@/layout/is-small';
+import MediumScreenNavigationBar from '@/app/[locale]/(main)/medium-navigation-items';
+import SmallScreenNavigationBar from '@/app/[locale]/(main)/small-navigation-items';
 
-const SmallScreenNavigationBar = dynamic(() => import('@/app/[locale]/(main)/small-navigation-items'), { ssr: false, loading: () => <div className="h-nav min-h-nav bg-brand" /> });
-const MediumScreenNavigationBar = dynamic(() => import('@/app/[locale]/(main)/medium-navigation-items'), { ssr: false, loading: () => <div className="w-nav bg-card border-r" /> });
+import { NavBarProvider } from '@/context/navbar-context';
 
 export default function NavigationBar({ children }: { children: ReactNode }) {
   return (
     <NavBarProvider>
       <div className="grid h-full w-full grid-rows-[var(--nav)_1fr] overflow-hidden z-50 sm:grid-cols-[auto_1fr] sm:justify-center sm:grid-rows-1">
-        <IsSmall
-          small={
-            <div className="flex h-nav w-full items-center justify-between bg-brand px-2 py-2 shadow-lg sm:hidden">
-              <SmallScreenNavigationBar />
-            </div>
-          }
-          notSmall={<MediumScreenNavigationBar />}
-        />
+        <div className="flex h-nav w-full items-center justify-between bg-brand px-2 py-2 shadow-lg sm:hidden">
+          <SmallScreenNavigationBar />
+        </div>
+        <MediumScreenNavigationBar />
         <div className="relative h-full w-full overflow-hidden">{children}</div>
       </div>
     </NavBarProvider>
