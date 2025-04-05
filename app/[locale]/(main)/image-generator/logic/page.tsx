263f01ab5cb10e486e69e5e48c63101f637c0bc0
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-05 17:42:55 +0700

    feat(image-generator): add image preview functionality
    Implement image preview feature using React Query and Axios for handling file uploads and displaying the uploaded image. This allows users to upload an image and see a preview before proceeding.

--- app/[locale]/(main)/image-generator/logic/page.tsx
+++ app/[locale]/(main)/image-generator/logic/page.tsx
@@ -1,3 +1,46 @@
+'use client'
+
+import { IMAGE_PREFIX } from "@/constant/constant";
+import useQueriesData from "@/hooks/use-queries-data";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+
+/* eslint-disable @next/next/no-img-element */
+
 export default function Page() {
-  return <div>Nothing there yet</div>;
+  const [file, setFile] = useState<File | null>(null)
+
+  const { invalidateByKey } = useQueriesData()
+
+  const { data: preview, isFetching, isError, error } = useQuery({
+    queryKey: ['image-preview'],
+    queryFn: async () => {
+      if (!file) return null;
+
+      const form = new FormData();
+      form.append('data', await file.bytes().then(v => new Buffer(v).toString('base64')));
+
+      const response = await axios.post(
+        'http://localhost:8081/api/v1/maps',
+        form,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          }
+        }
+      );
+
+      return response.data;
+    },
+    enabled: !!file,
+  });
+
+
+  return <div>
+    <button onClick={() => invalidateByKey(['image-preview'])}>Refersh</button>
+    <input type="file" onChange={event => setFile(event.currentTarget.files![0])} />
+    {isFetching && <div>Loading...</div>}
+    {preview && <img src={IMAGE_PREFIX + preview?.image} alt="map" />}
+  </div>;
 }
