1dbd6499229957c3baa8642bb5daee3ba0315411
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-06 16:32:36 +0700

    refactor: improve socket client cleanup and axios interceptors
    Refactor the socket client cleanup logic to handle the socket close operation more robustly by using `onopen` instead of `onConnect`. Additionally, reorganize the axios interceptors in `use-client.ts` to use `useEffect` for better lifecycle management and add error handling with toast notifications.

--- hooks/use-client.ts
+++ hooks/use-client.ts
@@ -1,6 +1,9 @@
 'use client';
 
 import { AxiosInstance } from 'axios';
+import { useEffect } from 'react';
+
+import { toast } from '@/components/ui/sonner';
 
 import { DEFAULT_PAGINATION_SIZE, PAGINATION_SIZE_PERSISTENT_KEY } from '@/context/session-context.type';
 import axiosInstance from '@/query/config/config';
@@ -20,25 +23,58 @@ export default function useClientApi(): AxiosInstance {
   axiosInstance.defaults.timeout = 60000;
 
   axiosInstance.interceptors.request.clear();
-  if (process.env.NODE_ENV !== 'production') {
-    axiosInstance.interceptors.request.use(async (config) => {
+
+  useEffect(() => {
+    const id = axiosInstance.interceptors.request.use(async (config) => {
+      const params = config.params;
+      if (!params || !('size' in params) || 'autoSize' in params) {
+        return config;
+      }
+
+      if (typeof window !== 'undefined') {
+        config.params['size'] = getCookie(PAGINATION_SIZE_PERSISTENT_KEY) ?? DEFAULT_PAGINATION_SIZE;
+      }
+
+      return config;
+    });
+
+    return () => {
+      axiosInstance.interceptors.request.eject(id);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      return;
+    }
+
+    const id = axiosInstance.interceptors.request.use(async (config) => {
       console.log(`CLIENT ${config.method?.toUpperCase()} ${config.baseURL}/${config.url}`);
       return config;
     });
-  }
 
-  axiosInstance.interceptors.request.use(async (config) => {
-    const params = config.params;
-    if (!params || !('size' in params) || 'autoSize' in params) {
-      return config;
-    }
+    return () => {
+      axiosInstance.interceptors.request.eject(id);
+    };
+  }, []);
 
-    if (typeof window !== 'undefined') {
-      config.params['size'] = getCookie(PAGINATION_SIZE_PERSISTENT_KEY) ?? DEFAULT_PAGINATION_SIZE;
-    }
+  useEffect(() => {
+    const id = axiosInstance.interceptors.response.use(
+      async (response) => {
+        return response;
+      },
 
-    return config;
-  });
+      (error) => {
+        if ('status' in error) {
+          toast.error(error.message);
+        }
+      },
+    );
+
+    return () => {
+      axiosInstance.interceptors.response.eject(id);
+    };
+  }, []);
 
   return axiosInstance;
 }
