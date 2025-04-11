56cd1ff57385c02ee1c4f1848c850a2a6d89f8dd
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-11 23:03:44 +0700

    refactor(hooks): integrate pagination size from cookies in useInfinitePageQuery
    This commit modifies the `useInfinitePageQuery` hook to use the pagination size stored in cookies, ensuring consistency across the application. The `getNextPageParam` and `getPreviousPageParam` functions now utilize this value for pagination calculations. Additionally, the code has been formatted for better readability.

--- components/common/grid-pagination-list.tsx
+++ components/common/grid-pagination-list.tsx
@@ -16,87 +16,100 @@ import { QuerySchema } from '@/query/search-query';
 import { QueryKey, useQuery } from '@tanstack/react-query';
 
 type Props<T, P extends QuerySchema> = {
-  className?: string;
-  queryKey: QueryKey;
-  paramSchema: P;
-  params?: Omit<z.infer<P>, 'page' | 'size'>;
-  loader?: ReactNode;
-  noResult?: ReactNode;
-  skeleton?: {
-    amount: number;
-    item: ReactNode | ((index: number) => ReactNode);
-  };
-  asChild?: boolean;
-  initialData?: T[];
-  initialParams?: z.infer<P>;
-  queryFn: (axios: AxiosInstance, params: z.infer<P>) => Promise<T[]>;
-  children: (data: T, index: number) => ReactNode;
+	className?: string;
+	queryKey: QueryKey;
+	paramSchema: P;
+	params?: Omit<z.infer<P>, 'page' | 'size'>;
+	loader?: ReactNode;
+	noResult?: ReactNode;
+	skeleton?: {
+		amount: number;
+		item: ReactNode | ((index: number) => ReactNode);
+	};
+	asChild?: boolean;
+	initialData?: T[];
+	initialParams?: z.infer<P>;
+	queryFn: (axios: AxiosInstance, params: z.infer<P>) => Promise<T[]>;
+	children: (data: T, index: number) => ReactNode;
 };
 
-export default function GridPaginationList<T, P extends QuerySchema>({ className, queryKey, paramSchema, loader, noResult, skeleton, asChild, initialData, initialParams, params, queryFn, children }: Props<T, P>) {
-  const p = useSearchQuery(paramSchema, params);
+export default function GridPaginationList<T, P extends QuerySchema>({
+	className,
+	queryKey,
+	paramSchema,
+	loader,
+	noResult,
+	skeleton,
+	asChild,
+	initialData,
+	initialParams,
+	params,
+	queryFn,
+	children,
+}: Props<T, P>) {
+	const p = useSearchQuery(paramSchema, params);
 
-  const axios = useClientApi();
-  const { data, error, isLoading } = useQuery({
-    queryFn: () => queryFn(axios, p),
-    queryKey: [p, ...queryKey],
-    initialData: JSON.stringify(p) === JSON.stringify(initialParams) ? initialData : undefined,
-  });
+	const axios = useClientApi();
+	const { data, error, isLoading } = useQuery({
+		queryFn: () => queryFn(axios, p),
+		queryKey: [p, ...queryKey],
+		initialData: JSON.stringify(p) === JSON.stringify(initialParams) ? initialData : undefined,
+	});
 
-  const skeletonElements = useMemo(() => {
-    if (skeleton)
-      return Array(skeleton.amount)
-        .fill(1)
-        .map((_, index) => <React.Fragment key={index}>{typeof skeleton.item === 'function' ? skeleton.item(index) : skeleton.item}</React.Fragment>);
-  }, [skeleton]);
+	const skeletonElements = useMemo(() => {
+		if (skeleton)
+			return Array(skeleton.amount)
+				.fill(1)
+				.map((_, index) => <React.Fragment key={index}>{typeof skeleton.item === 'function' ? skeleton.item(index) : skeleton.item}</React.Fragment>);
+	}, [skeleton]);
 
-  noResult = useMemo(() => noResult ?? <NoResult className="flex w-full items-center justify-center" />, [noResult]);
-  loader = useMemo(() => (!loader && !skeleton ? <LoadingSpinner key="loading" className="col-span-full flex h-full w-full items-center justify-center" /> : undefined), [loader, skeleton]);
+	noResult = useMemo(() => noResult ?? <NoResult className="flex w-full items-center justify-center" />, [noResult]);
+	loader = useMemo(() => (!loader && !skeleton ? <LoadingSpinner key="loading" className="col-span-full flex h-full w-full items-center justify-center" /> : undefined), [loader, skeleton]);
 
-  if (asChild) {
-    return (
-      <Render isLoading={isLoading} loader={loader} skeletonElements={skeletonElements} error={error} data={data} noResult={noResult}>
-        {children}
-      </Render>
-    );
-  }
+	if (asChild) {
+		return (
+			<Render isLoading={isLoading} loader={loader} skeletonElements={skeletonElements} error={error} data={data} noResult={noResult}>
+				{children}
+			</Render>
+		);
+	}
 
-  return (
-    <div className="scroll-container h-full">
-      <section className={cn('grid w-full grid-cols-[repeat(auto-fill,minmax(min(var(--preview-size),100%),1fr))] justify-center gap-2', className)}>
-        <Render isLoading={isLoading} loader={loader} skeletonElements={skeletonElements} error={error} data={data} noResult={noResult}>
-          {children}
-        </Render>
-      </section>
-    </div>
-  );
+	return (
+		<div className="scroll-container h-full">
+			<section className={cn('grid w-full grid-cols-[repeat(auto-fill,minmax(min(var(--preview-size),100%),1fr))] justify-center gap-2', className)}>
+				<Render isLoading={isLoading} loader={loader} skeletonElements={skeletonElements} error={error} data={data} noResult={noResult}>
+					{children}
+				</Render>
+			</section>
+		</div>
+	);
 }
 
 type RenderProps<T> = {
-  isLoading: boolean;
-  loader: ReactNode;
-  skeletonElements?: React.JSX.Element[];
-  error: any;
-  data?: T[];
-  noResult: ReactNode;
-  children: (data: T, index: number) => ReactNode;
+	isLoading: boolean;
+	loader: ReactNode;
+	skeletonElements?: React.JSX.Element[];
+	error: any;
+	data?: T[];
+	noResult: ReactNode;
+	children: (data: T, index: number) => ReactNode;
 };
 function Render<T>({ isLoading, loader, skeletonElements, error, data, noResult, children }: RenderProps<T>) {
-  if (isLoading) {
-    return loader ? loader : skeletonElements;
-  }
+	if (isLoading) {
+		return loader ? loader : skeletonElements;
+	}
 
-  if (error) {
-    return (
-      <div className="col-span-full flex h-full w-full justify-center">
-        <RouterSpinner message={error?.message} />
-      </div>
-    );
-  }
+	if (error) {
+		return (
+			<div className="col-span-full flex h-full w-full justify-center">
+				<RouterSpinner message={error?.message} />
+			</div>
+		);
+	}
 
-  if (!data) {
-    return noResult;
-  }
+	if (!data) {
+		return noResult;
+	}
 
-  return data.map((item, index) => children(item, index));
+	return data.map((item, index) => children(item, index));
 }
