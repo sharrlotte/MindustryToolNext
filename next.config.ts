858f70578f518d03165bc8cb77ae47552da7d710
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-20 19:42:55 +0700

    refactor(next.config.ts): restructure config for better maintainability
    The config logic was moved into a separate `config` variable and conditionally applied based on the environment. This improves readability and makes the configuration easier to manage.

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,61 @@ import bundleAnalyzer from '@next/bundle-analyzer';
 import createMDX from '@next/mdx';
 import { withSentryConfig } from '@sentry/nextjs';
 
+const config: NextConfig =
+	process.env.NODE_ENV === 'development'
+		? {}
+		: {
+				logging: {
+					fetches: {
+						fullUrl: true,
+					},
+				},
+				async headers() {
+					return [
+						{
+							source: '/(.*)',
+							headers: [
+								{
+									key: 'X-Content-Type-Options',
+									value: 'nosniff',
+								},
+								{
+									key: 'X-Frame-Options',
+									value: 'DENY',
+								},
+								{
+									key: 'Referrer-Policy',
+									value: 'strict-origin-when-cross-origin',
+								},
+							],
+						},
+						{
+							source: '/sw.js',
+							headers: [
+								{
+									key: 'Content-Type',
+									value: 'application/javascript; charset=utf-8',
+								},
+								{
+									key: 'Cache-Control',
+									value: 'no-cache, no-store, must-revalidate',
+								},
+								{
+									key: 'Content-Security-Policy',
+									value: "default-src 'self'; script-src 'self'",
+								},
+							],
+						},
+					];
+				},
+			};
+
 const nextConfig: NextConfig = {
+	...config,
 	experimental: {
 		reactCompiler: true,
 	},
 	pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-	logging: {
-		fetches: {
-			fullUrl: true,
-		},
-	},
 	crossOrigin: 'anonymous',
 	reactStrictMode: true,
 	images: {
@@ -52,44 +97,6 @@ const nextConfig: NextConfig = {
 				pathname: '/**',
 			},
 		],
-	},
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff',
-					},
-					{
-						key: 'X-Frame-Options',
-						value: 'DENY',
-					},
-					{
-						key: 'Referrer-Policy',
-						value: 'strict-origin-when-cross-origin',
-					},
-				],
-			},
-			{
-				source: '/sw.js',
-				headers: [
-					{
-						key: 'Content-Type',
-						value: 'application/javascript; charset=utf-8',
-					},
-					{
-						key: 'Cache-Control',
-						value: 'no-cache, no-store, must-revalidate',
-					},
-					{
-						key: 'Content-Security-Policy',
-						value: "default-src 'self'; script-src 'self'",
-					},
-				],
-			},
-		];
 	},
 };
 
