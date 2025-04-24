1a157bac20a555aa800159292d9ec557ac40efce
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-25 00:51:01 +0700

    style: remove extra blank lines in server update forms
    This commit removes unnecessary blank lines in the server update form components to improve code readability and maintain consistency in the codebase. The changes do not affect the functionality of the forms.

--- app/[locale]/(main)/servers/[id]/setting/server-update-form.tsx
+++ app/[locale]/(main)/servers/[id]/setting/server-update-form.tsx
@@ -3,8 +3,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-
-
 import ColorText from '@/components/common/color-text';
 import ComboBox from '@/components/common/combo-box';
 import Tran from '@/components/common/tran';
@@ -14,8 +12,6 @@ import { Input } from '@/components/ui/input';
 import { toast } from '@/components/ui/sonner';
 import { Textarea } from '@/components/ui/textarea';
 
-
-
 import { revalidate } from '@/action/common';
 import useClientApi from '@/hooks/use-client';
 import useQueriesData from '@/hooks/use-queries-data';
@@ -24,11 +20,8 @@ import { updateServer } from '@/query/server';
 import { PutServerRequest, PutServerSchema, ServerModes } from '@/types/request/UpdateServerRequest';
 import Server from '@/types/response/Server';
 
-
-
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQuery } from '@tanstack/react-query';
-
 
 type Props = {
 	server: Server;
