1a8282dc065ddcedd68da7ae99070904dce59634
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-28 01:46:54 +0700

    style: increase padding for file list items in upload admin page
    This change improves the visual spacing of file list items by increasing the padding from `p-1` to `p-2`, enhancing readability and user experience.

--- app/[locale]/(main)/upload/admin/page.tsx
+++ app/[locale]/(main)/upload/admin/page.tsx
@@ -174,7 +174,7 @@ function List({ state }: { state: UploadState }) {
 			<h3 className="font-semibold mb-2">{state}</h3>
 			<div className="w-full space-y-1">
 				{data?.map((file) => (
-					<div className="rounded-md border p-1 bg-card" key={file}>
+					<div className="rounded-md border p-2 bg-card" key={file}>
 						{file}
 					</div>
 				))}
