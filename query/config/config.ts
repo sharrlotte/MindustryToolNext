9a3f0ab2239e7b2fdcc9933a1786fac1f771139d
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-04-27 19:27:15 +0700

    refactor(query/config): simplify error handling in axios interceptor
    Remove custom error handling logic and directly reject the error in the axios interceptor. This reduces complexity and aligns with standard error handling practices.

--- query/config/config.ts
+++ query/config/config.ts
@@ -1,38 +1,6 @@
-import Axios, { AxiosError } from 'axios';
+import Axios from 'axios';
 
 import env from '@/constant/env';
-
-class StatusError extends Error {
-	constructor(
-		public status: number,
-		public message: string,
-		public originalError: unknown,
-	) {
-		super(message);
-		this.name = 'StatusError';
-	}
-}
-
-function createApiError(error: AxiosError<any, { status: string; message: string }>) {
-	return new StatusError(error.response?.data?.status, error.response?.data?.message, error);
-}
-
-function createNetworkError(error: unknown) {
-	const message = !navigator?.onLine ? 'You are offline' : 'Network error';
-	return new StatusError(503, message, error);
-}
-
-function createServiceError(error: unknown) {
-	return new StatusError(503, 'Service is unavailable, please try again later', error);
-}
-
-function createAxiosError(error: AxiosError) {
-	return new StatusError(error.status ?? 500, `Axios error: ${error?.message}`, error);
-}
-
-function createUnknownError(error: unknown) {
-	return new StatusError(500, `An unknown error occurred: ${(error as Error).message}`, error);
-}
 
 function logError(error: unknown) {
 	try {
@@ -62,25 +30,7 @@ axiosInstance.interceptors.response.use(
 	(error) => {
 		logError(error);
 
-		if (error?.message) {
-			return Promise.reject(error?.message);
-		}
-
-		let statusError: StatusError;
-
-		if (error?.response?.data) {
-			statusError = createApiError(error);
-		} else if (error.errno === -4078) {
-			statusError = createServiceError(error);
-		} else if ('code' in error && error.code === 'ERR_NETWORK') {
-			statusError = createNetworkError(error);
-		} else if (Axios.isAxiosError(error)) {
-			statusError = createAxiosError(error);
-		} else {
-			statusError = createUnknownError(error);
-		}
-
-		return Promise.reject(statusError);
+		return Promise.reject(error);
 	},
 );
 
