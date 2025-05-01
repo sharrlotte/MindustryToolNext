5f1a685aac305c95ba70ee8569d782db87067110
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-01 20:33:35 +0700

    refactor(alert-dialog): adjust z-index and remove redundant overlay
    The z-index of the AlertDialogOverlay was reduced from 50 to 10 to better align with the component hierarchy. Additionally, the redundant AlertDialogOverlay with a z-index of 40 was removed to simplify the structure and avoid potential stacking issues.

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -19,7 +19,7 @@ const AlertDialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<AlertDialogPrimitive.Overlay
 		className={cn(
-			'fixed inset-0 z-50 bg-black/90 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+			'fixed inset-0 z-10 bg-black/90 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
 			className,
 		)}
 		{...props}
@@ -33,7 +33,6 @@ const AlertDialogContent = React.forwardRef<
 	React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Content>
 >(({ className, ...props }, ref) => (
 	<AlertDialogPortal>
-		<AlertDialogOverlay className="z-40" />
 		<AlertDialogPrimitive.Content
 			ref={ref}
 			className={cn(
