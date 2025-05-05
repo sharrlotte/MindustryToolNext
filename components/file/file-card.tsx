fe95a15ef4321fc35c6bc558b107bc77a720c433
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-05 10:12:30 +0700

    style: remove unnecessary padding and adjust indentation
    Remove padding from the HomeIcon in FileHierarchy and standardize indentation across components. Also, add a Divider in the files page for better visual separation.

--- components/file/file-card.tsx
+++ components/file/file-card.tsx
@@ -7,29 +7,29 @@ import { byteToSize } from '@/lib/utils';
 import { ServerFile } from '@/types/response/ServerFile';
 
 type Props = {
-  file: ServerFile;
-  onClick: (file: ServerFile) => void;
-  children: ReactNode;
+	file: ServerFile;
+	onClick: (file: ServerFile) => void;
+	children: ReactNode;
 };
 
 export default function FileCard({ file, children, onClick }: Props) {
-  const { data, name, size, directory } = file;
+	const { data, name, size, directory } = file;
 
-  if (data) return <p className="whitespace-pre-line">{data}</p>;
+	if (data) return <p className="whitespace-pre-line">{data}</p>;
 
-  return (
-    <ContextMenu>
-      <ContextMenuTrigger
-        className="flex h-10 cursor-pointer items-center justify-start gap-1 rounded-md bg-card p-2 text-sm"
-        onClick={() => {
-          if (size <= 5000000) onClick(file);
-        }}
-      >
-        {directory ? <FolderIcon /> : <FileIcon />}
-        <span>{name}</span>
-        {!directory && <span className="ml-auto"> {byteToSize(size)}</span>}
-      </ContextMenuTrigger>
-      <ContextMenuContent>{children}</ContextMenuContent>
-    </ContextMenu>
-  );
+	return (
+		<ContextMenu>
+			<ContextMenuTrigger
+				className="flex h-10 cursor-pointer items-center justify-start gap-1 rounded-md bg-secondary border p-2 text-sm"
+				onClick={() => {
+					if (size <= 5000000) onClick(file);
+				}}
+			>
+				{directory ? <FolderIcon /> : <FileIcon />}
+				<span>{name}</span>
+				{!directory && <span className="ml-auto"> {byteToSize(size)}</span>}
+			</ContextMenuTrigger>
+			<ContextMenuContent>{children}</ContextMenuContent>
+		</ContextMenu>
+	);
 }
