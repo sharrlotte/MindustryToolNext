d116eec5508038b01860e2c9a453cacd2a0c284c
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-13 15:29:35 +0700

    refactor(documents): remove unnecessary API error display
    The API error display was redundant and cluttered the UI. Simplified the page by removing the error message and the `ColorText` component import, resulting in a cleaner and more maintainable codebase.

--- app/[locale]/documents/page.tsx
+++ app/[locale]/documents/page.tsx
@@ -1,13 +1,3 @@
-import ColorText from '@/components/common/color-text';
-
 export default function Page() {
-	return (
-		<div>
-			<div>
-				[cyan][API][] [7cce9805-0bfd-4936-8338-9a778624b8c4] [yellow]401[] mindustrytool.api.v3.exception.ApiError
-				http://api.mindustry-tool.com/socket [Anonymous] (85.140.93.118): unauthorized
-			</div>
-			<ColorText text="[cyan][API][] [7cce9805-0bfd-4936-8338-9a778624b8c4] [yellow]401[] mindustrytool.api.v3.exception.ApiError http://api.mindustry-tool.com/socket [Anonymous] (85.140.93.118): unauthorized" />
-		</div>
-	);
+	return <div></div>;
 }
