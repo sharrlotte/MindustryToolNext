a4cc5b7a36b622e5b30ffa99585b8be33600e92d
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-15 03:39:45 +0700

    refactor(markdown): extract MarkdownImage component for better maintainability
    The MarkdownImage component was extracted from the main markdown.tsx file into a separate file to improve code organization and maintainability. This change also includes updating the documents page to use the Markdown component for rendering rules.

--- app/[locale]/documents/page.tsx
+++ app/[locale]/documents/page.tsx
@@ -1,9 +1,32 @@
 import ColorText from '@/components/common/color-text';
+import Markdown from '@/components/markdown/markdown';
 
 export default function Page() {
 	return (
 		<div>
-			<ColorText text="[#0073E]V[#00BFF]N[#00FFF]M [#FF149]PvP [] " />
+			<Markdown>{`
+			### ***Rules:***
+
+_1. No toxicity or bullying towards others._
+
+_2. No spamming or intentionally disturbing other members._
+
+_3. No NSFW, offensive, or violent content._
+
+_4. Use channels and commands appropriately._
+
+_5. Do not engage with rule-breakers — report them instead._
+
+_6. Avoid discussing politics, religion, racism, or sexism._
+
+_7. Do not mention roles unnecessarily._
+
+_8. No advertising or posting links without admin approval._
+
+_9. Do not exploit or take advantage of others' efforts._
+
+> ***Violations of these rules may result in disciplinary actions depending on severity.***
+			`}</Markdown>
 		</div>
 	);
 }
--- components/markdown/markdown.tsx
+++ components/markdown/markdown.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { useState } from 'react';
+import React from 'react';
 import { isValidElement } from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
@@ -8,9 +7,8 @@ import frontmatter from 'remark-frontmatter';
 import remarkGfm from 'remark-gfm';
 
 import CopyButton from '@/components/button/copy.button';
-import { Hidden } from '@/components/common/hidden';
 import InternalLink from '@/components/common/internal-link';
-import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import MarkdownImage from '@/components/markdown/markdown-image';
 
 import env from '@/constant/env';
 import { YOUTUBE_VIDEO_REGEX, cn, extractYouTubeID } from '@/lib/utils';
@@ -79,45 +77,6 @@ function A({ href, children }: any) {
 		<InternalLink className="text-brand" href={href}>
 			{children}
 		</InternalLink>
-	);
-}
-
-function MarkdownImage({ src, alt }: any) {
-	const [isError, setError] = useState(false);
-
-	if (src && src.includes(env.url.base)) {
-		src = src && 'blob:' + src.substring(0, src.lastIndexOf('.'));
-	}
-
-	src = src.replace('image.mindustry-tool.app', 'image.mindustry-tool.com');
-
-	if (isError) {
-		return alt;
-	}
-
-	const width = new URL(src).searchParams.get('w') ?? undefined;
-	const height = new URL(src).searchParams.get('h') ?? undefined;
-
-	return (
-		<Dialog>
-			<DialogTrigger asChild>
-				<img
-					className="markdown-image rounded-md max-h-[50dvh]"
-					alt={alt}
-					src={src}
-					width={width}
-					height={height}
-					onError={() => setError(true)}
-				/>
-			</DialogTrigger>
-			<DialogContent className="max-w-[100dvw] max-h-dvh sm:max-h-dvh flex justify-center items-center h-full">
-				<Hidden>
-					<DialogTitle />
-					<DialogDescription />
-				</Hidden>
-				<img alt={alt} src={src} onError={() => setError(true)} />
-			</DialogContent>
-		</Dialog>
 	);
 }
 
