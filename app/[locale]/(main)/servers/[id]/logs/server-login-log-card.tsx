11b9dde170911f79d0b3731c4007ab0b325c2400
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-18 08:07:02 +0700

    refactor(visibility): replace manual visibility toggles with reusable Visibility component
    This commit replaces the manual implementation of visibility toggles for sensitive information (UUIDs and IPs) with a reusable `Visibility` component. This improves code maintainability and consistency across the application. The `Visibility` component handles the toggling logic internally, reducing redundancy and potential errors in the UI.

--- app/[locale]/(main)/servers/[id]/logs/server-login-log-card.tsx
+++ app/[locale]/(main)/servers/[id]/logs/server-login-log-card.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { useState } from 'react';
-
 import ColorText from '@/components/common/color-text';
-import { EyeIcon, EyeOffIcon } from '@/components/common/icons';
 import { RelativeTime } from '@/components/common/relative-time';
+import { Visibility, VisibilityOff, VisibilityOn } from '@/components/common/visibility';
 import { BanButton } from '@/components/server/ban.button';
 import { KickButton } from '@/components/server/kick.button';
 import { EllipsisButton } from '@/components/ui/ellipsis-button';
@@ -19,9 +17,6 @@ type ServerLoginLogCardProps = {
 };
 
 export default function ServerLoginLogCard({ serverId, data: { name, uuid, ip, createdAt }, index }: ServerLoginLogCardProps) {
-	const [showUuid, setShowUuid] = useState(false);
-	const [showIp, setShowIp] = useState(false);
-
 	return (
 		<div className={cn('justify-between gap-2 text-sm flex border rounded-md', index % 2 === 0 ? 'bg-card/50' : 'bg-card')}>
 			<div className="grid-cols-1 grid p-2 gap-2 w-full">
@@ -31,23 +26,17 @@ export default function ServerLoginLogCard({ serverId, data: { name, uuid, ip, c
 				</span>
 				<div className="flex items-center gap-2 group">
 					<span className="font-semibold">UUID: </span>
-					{showUuid ? uuid : '*'.repeat(27)}
-					<button
-						className="group-hover:opacity-100 group-focus:opacity-100 opacity-0"
-						onClick={() => setShowUuid((prev) => !prev)}
-					>
-						{showUuid ? <EyeOffIcon /> : <EyeIcon />}
-					</button>
+					<Visibility>
+						<VisibilityOff>{'*'.repeat(27)}</VisibilityOff>
+						<VisibilityOn>{uuid}</VisibilityOn>
+					</Visibility>
 				</div>
 				<div className="flex items-center gap-2 group">
 					<span className="font-semibold">IP: </span>
-					{showIp ? ip : ip.replaceAll(/\d/g, '*')}
-					<button
-						className="group-hover:opacity-100 group-focus:opacity-100 opacity-0"
-						onClick={() => setShowIp((prev) => !prev)}
-					>
-						{showIp ? <EyeOffIcon /> : <EyeIcon />}
-					</button>
+					<Visibility>
+						<VisibilityOff>{ip.replaceAll(/\d/g, '*')}</VisibilityOff>
+						<VisibilityOn>{ip}</VisibilityOn>
+					</Visibility>
 				</div>
 				<RelativeTime date={new Date(createdAt)} />
 			</div>
