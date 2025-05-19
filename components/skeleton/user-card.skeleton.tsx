d0efb8538173c00be1b59617b39fefbb9a03b6d7
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-20 06:59:30 +0700

    style(skeleton): adjust height and spacing in user-card skeleton
    Update the height and spacing in the user-card skeleton component to improve visual consistency and alignment with the design system.

--- components/skeleton/user-card.skeleton.tsx
+++ components/skeleton/user-card.skeleton.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function UserCardSkeleton({ avatar }: { avatar?: boolean }) {
 	return (
-		<div className="flex min-h-8 h-8 w-56 items-end justify-start gap-2">
+		<div className="flex min-h-5 w-56 items-end justify-start gap-2">
 			{avatar && <Skeleton className="block h-8 w-8 rounded-full border border-border" />}
-			<Skeleton className="h-full w-full flex-1 rounded-md" />
+			<Skeleton className="h-5 w-full flex-1 rounded-md" />
 		</div>
 	);
 }
