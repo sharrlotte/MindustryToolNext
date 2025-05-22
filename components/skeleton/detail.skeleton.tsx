794a38009d361aff042aa8d7473837d6c738e2de
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-23 01:36:21 +0700

    style(components): reorder class names for consistency and readability
    Reordered class names in multiple components to maintain a consistent and readable structure. This change does not affect the functionality but improves the code maintainability and aligns with the project's styling conventions.

--- components/skeleton/detail.skeleton.tsx
+++ components/skeleton/detail.skeleton.tsx
@@ -6,24 +6,24 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function DetailSkeleton() {
 	return (
-		<div className="absolute h-full w-full bg-background p-2 backdrop-blur-sm">
-			<div className="relative h-full flex flex-col lg:grid lg:grid-cols-[1fr_400px] lg:divide-x overflow-auto">
+		<div className="absolute p-2 w-full h-full backdrop-blur-sm bg-background">
+			<div className="relative h-full flex flex-col lg:grid lg:grid-cols-[1fr_500px] lg:divide-x overflow-auto">
 				<div className="p-2 h-full overflow-auto flex justify-center max-h-[50vh] lg:max-h-full">
-					<Skeleton className="object-cover w-full aspect-square rounded-lg" />
+					<Skeleton className="object-cover w-full rounded-lg aspect-square" />
 				</div>
 				<div className="flex flex-col gap-2 p-2 h-full">
-					<Skeleton className="h-12 w-full rounded-md" />
+					<Skeleton className="w-full h-12 rounded-md" />
 					<Divider />
 					<UserCardSkeleton />
 					<Skeleton className="h-8 w-[80%] rounded-md" />
-					<Skeleton className="h-8 w-full rounded-md" />
+					<Skeleton className="w-full h-8 rounded-md" />
 					<Skeleton className="h-8 w-[70%] rounded-md" />
-					<Skeleton className="h-8 w-full rounded-md" />
-					<div className="grid gap-2 mt-auto grid-cols-4 border-t pt-2">
-						<Skeleton className="h-8 w-full rounded-md" />
-						<Skeleton className="h-8 w-full rounded-md" />
-						<Skeleton className="h-8 w-full rounded-md" />
-						<Skeleton className="h-8 w-full rounded-md" />
+					<Skeleton className="w-full h-8 rounded-md" />
+					<div className="grid grid-cols-4 gap-2 pt-2 mt-auto border-t">
+						<Skeleton className="w-full h-8 rounded-md" />
+						<Skeleton className="w-full h-8 rounded-md" />
+						<Skeleton className="w-full h-8 rounded-md" />
+						<Skeleton className="w-full h-8 rounded-md" />
 					</div>
 				</div>
 			</div>
