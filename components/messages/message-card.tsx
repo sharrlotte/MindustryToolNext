cfcc05e281272fa247dcc4fa04af567b5ff64219
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-24 05:12:35 +0700

    style: reformat indentation in message-card.tsx for consistency
    The indentation in the message-card.tsx file was updated to use tabs instead of spaces to maintain consistency with the project's coding standards. This change does not affect the functionality of the code.

--- components/messages/message-card.tsx
+++ components/messages/message-card.tsx
@@ -13,62 +13,66 @@ import { MessageGroup } from '@/types/response/Message';
 import { useQuery } from '@tanstack/react-query';
 
 type Props = {
-  className?: string;
-  message: MessageGroup;
+	className?: string;
+	message: MessageGroup;
 };
 
 export function MessageCard({ className, message }: Props) {
-  const { userId, contents, createdAt } = message;
+	const { userId, contents, createdAt } = message;
 
-  const { data } = useQuery({
-    queryKey: ['users', userId],
-    queryFn: () => Batcher.user.get(userId),
-    persister,
-  });
+	const { data } = useQuery({
+		queryKey: ['users', userId],
+		queryFn: () => Batcher.user.get(userId),
+		persister,
+	});
 
-  return (
-    <div className={cn('flex gap-2 p-2 w-full text-base rounded-lg text-wrap hover:bg-secondary/50', className)}>
-      {data ? <UserAvatar url={`/users/${userId}`} user={data} /> : <Skeleton className="flex justify-center items-center capitalize rounded-full border size-8 min-h-8 min-w-8 border-border" />}
-      <div className="overflow-hidden">
-        <div className="flex gap-2">
-          {data ? (
-            <ColorAsRole className="font-semibold capitalize" roles={data.roles}>
-              {data.name}
-            </ColorAsRole>
-          ) : (
-            <Skeleton className="w-24 h-4 max-h-4" />
-          )}
-          <RelativeTime className='text-muted-foreground' date={new Date(createdAt)} />
-        </div>
-        <div className="grid overflow-hidden w-full no-scrollbar">
-          <TooltipProvider>
-            {contents.map(({ text, createdAt }, index) => (
-              <Tooltip key={index}>
-                <TooltipTrigger className="flex justify-start items-start p-0 cursor-default pointer-events-auto select-text text-start w-fit">
-                  <ColorText className="overflow-hidden text-base break-words" text={text} />
-                </TooltipTrigger>
-                <TooltipContent>{new Date(createdAt).toLocaleString()}</TooltipContent>
-              </Tooltip>
-            ))}
-          </TooltipProvider>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={cn('flex gap-2 p-2 w-full text-base rounded-lg text-wrap hover:bg-secondary/50', className)}>
+			{data ? (
+				<UserAvatar url={`/users/${userId}`} user={data} />
+			) : (
+				<Skeleton className="flex justify-center items-center capitalize rounded-full border size-8 min-h-8 min-w-8 border-border" />
+			)}
+			<div className="overflow-hidden">
+				<div className="flex gap-2">
+					{data ? (
+						<ColorAsRole className="capitalize" roles={data.roles}>
+							{data.name}
+						</ColorAsRole>
+					) : (
+						<Skeleton className="w-24 h-4 max-h-4" />
+					)}
+					<RelativeTime className="text-muted-foreground" date={new Date(createdAt)} />
+				</div>
+				<div className="grid overflow-hidden w-full no-scrollbar">
+					<TooltipProvider>
+						{contents.map(({ text, createdAt }, index) => (
+							<Tooltip key={index}>
+								<TooltipTrigger className="flex justify-start items-start p-0 cursor-default pointer-events-auto select-text text-start w-fit">
+									<ColorText className="overflow-hidden text-base break-words" text={text} />
+								</TooltipTrigger>
+								<TooltipContent>{new Date(createdAt).toLocaleString()}</TooltipContent>
+							</Tooltip>
+						))}
+					</TooltipProvider>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export function MessageCardSkeleton() {
-  return (
-    <div className="flex gap-2 p-2 w-full h-16 text-base rounded-lg text-wrap">
-      <Skeleton className="flex justify-center items-center capitalize rounded-full border size-8 min-h-8 min-w-8 border-border" />
-      <div className="overflow-hidden">
-        <div className="flex gap-2">
-          <Skeleton className="w-24 h-4 max-h-4" />
-        </div>
-        <div className="grid overflow-hidden gap-1 w-full no-scrollbar">
-          <Skeleton className="w-full h-6 rounded-md" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex gap-2 p-2 w-full h-16 text-base rounded-lg text-wrap">
+			<Skeleton className="flex justify-center items-center capitalize rounded-full border size-8 min-h-8 min-w-8 border-border" />
+			<div className="overflow-hidden">
+				<div className="flex gap-2">
+					<Skeleton className="w-24 h-4 max-h-4" />
+				</div>
+				<div className="grid overflow-hidden gap-1 w-full no-scrollbar">
+					<Skeleton className="w-full h-6 rounded-md" />
+				</div>
+			</div>
+		</div>
+	);
 }
