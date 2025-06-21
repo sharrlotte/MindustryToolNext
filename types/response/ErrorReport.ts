a5ad536d5692c21539a9a780d4656b1d3998218a
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-06-21 12:14:08 +0700

    refactor: update ErrorReportSchema to define content structure with specific fields

--- types/response/ErrorReport.ts
+++ types/response/ErrorReport.ts
@@ -6,7 +6,12 @@ export const ErrorStatusSchema = z.enum(errorStatus);
 
 export const ErrorReportSchema = z.object({
 	id: z.string(),
-	content: z.any(),
+	content: z.object({
+		errors: z.string(),
+		message: z.string().nullable(),
+		url: z.string(),
+		user: z.string(),
+	}),
 	ip: z.string(),
 	status: z.enum(errorStatus),
 	createdAt: z.number(),
