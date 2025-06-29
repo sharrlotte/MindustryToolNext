7124e101e2f2bfcbc3db6d6a55deeeb0b86fc6bd
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-06-29 17:17:10 +0700

    fix: update MindustryToolIcon to use correct image source URL

--- components/common/mindustrytool-icon.tsx
+++ components/common/mindustrytool-icon.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
 export const MindustryToolIcon = ({ className, width, height }: { className?: string; width?: number; height?: number }) => (
-	<Image className={className} width={width ?? 32} height={height ?? 32} src={'/favicon.ico'} alt="MindustryToolIcon" />
+	<Image
+		className={className}
+		width={width ?? 32}
+		height={height ?? 32}
+		src="https://image.mindustry-tool.com//docs/icon.png
+"
+		alt="MindustryToolIcon"
+	/>
 );
